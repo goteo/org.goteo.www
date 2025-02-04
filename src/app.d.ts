92b5886261b8093849c89517377f4f24470f10b6
@milesibastos <antonio@milesibastos.com>
2025-02-04 12:11:08 -0300

    feat: add locale support to Locals interface in app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
+import type { SupportedLocale } from '$lib/i18n';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      locale: SupportedLocale;
+    }
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
