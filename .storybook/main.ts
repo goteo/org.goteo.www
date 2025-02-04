816386f11453f733bf087cfb0162e8511f28e010
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-02-04 12:59:24 +0100

    Load storybook stories from src/lib/components

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from "@storybook/sveltekit";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|ts|svelte)"],
+  stories: [
+    // "../src/lib/components/**/*.mdx",
+    "../src/lib/components/**/*.stories.@(js|ts|svelte)"],
   addons: [
     "@storybook/addon-svelte-csf",
     "@storybook/addon-essentials",
