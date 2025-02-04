be908bffd9fb0c23530656bb3903502cb4daf2a4
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-02-04 12:59:39 +0100

    Start base header component with story

---
+++ src/lib/components/Header/Header.stories.svelte
@@ -0,0 +1,10 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import Header from "./Header.svelte";
+
+  const { Story } = defineMeta({
+    component: Header,
+  });
+</script>
+
+<Story name="Base" />
---
+++ src/lib/components/Header/Header.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Logo from "./Logo.svg";
+</script>
+
+<header class="w-100% py-6 px-10 bg-[#59e9d3]">
+  <img alt="Goteo" src={Logo}>
+</header>
