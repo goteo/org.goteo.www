fe9e1b7b41ec128e300fdfd6b9dd5970ddbe5f8a
@milesibastos <antonio@milesibastos.com>
2025-02-11 08:05:38 -0300

    feat: add Dialog component with customizable content and integration in ProjectBudget
    npx shadcn-svelte@latest add dialog

--- src/lib/components/ProjectBudget/ProjectBudget.svelte
+++ src/lib/components/ProjectBudget/ProjectBudget.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import * as Card from "$lib/components/ui/card";
   import { _ } from "svelte-i18n";
+  import * as Card from "$lib/components/ui/card";
+  import * as Dialog from "$lib/components/ui/dialog";
 
   export let header: string = "";
   export let content: string = "";
@@ -8,21 +9,44 @@
   export let target: number = 0;
 </script>
 
-<Card.Root class="w-full max-w-md drop-shadow-sm">
-  <Card.Header>
-    <Card.Title>{header}</Card.Title>
-  </Card.Header>
-  <Card.CardContent class="p-6 space-y-6">
-    <Card.Description>{content}</Card.Description>
-  </Card.CardContent>
-  <Card.Footer class="flex gap-8">
-    <div>
-      <p class="text-base font-medium text-gray-500">{$_("campaignProgress.minimum")}</p>
-      <p class="text-3xl font-bold">{minimum.toLocaleString()}€</p>
+<Dialog.Root>
+  <Dialog.Trigger class="text-left">
+    <Card.Root class="w-full max-w-md drop-shadow-sm">
+      <Card.Header>
+        <Card.Title>{header}</Card.Title>
+      </Card.Header>
+      <Card.CardContent>
+        <Card.Description>{content?.slice(0, 170)}...</Card.Description>
+      </Card.CardContent>
+
+      <Card.Footer class="flex gap-8">
+        <div>
+          <p class="text-base font-medium text-gray-500">{$_("campaignProgress.minimum")}</p>
+          <p class="text-xl font-bold">{minimum.toLocaleString()}€</p>
+        </div>
+        <div>
+          <p class="text-base font-medium text-gray-500">{$_("campaignProgress.optimal")}</p>
+          <p class="text-xl font-bold">{target.toLocaleString()}€</p>
+        </div>
+      </Card.Footer>
+    </Card.Root>
+  </Dialog.Trigger>
+  <Dialog.Content>
+    <Dialog.Header>
+      <Dialog.Title>{header}</Dialog.Title>
+    </Dialog.Header>
+    <Dialog.Description>
+      {content}
+    </Dialog.Description>
+    <div class="flex justify-start gap-8">
+      <div>
+        <p class="text-base font-medium text-gray-500">{$_("campaignProgress.minimum")}</p>
+        <p class="text-xl font-bold">{minimum.toLocaleString()}€</p>
+      </div>
+      <div>
+        <p class="text-base font-medium text-gray-500">{$_("campaignProgress.optimal")}</p>
+        <p class="text-xl font-bold">{target.toLocaleString()}€</p>
+      </div>
     </div>
-    <div>
-      <p class="text-base font-medium text-gray-500">{$_("campaignProgress.optimal")}</p>
-      <p class="text-3xl font-bold">{target.toLocaleString()}€</p>
-    </div>
-  </Card.Footer>
-</Card.Root>
+  </Dialog.Content>
+</Dialog.Root>
