d517ae506e7d9757543b0653e0423e7b50183027
@milesibastos <antonio@milesibastos.com>
2025-02-14 11:09:08 -0300

    feat: add Header component with improved layout and styling, and update imports in layout

--- src/routes/[[locale]]/+layout.svelte
+++ src/routes/[[locale]]/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
   import { isLoading, locale } from "svelte-i18n";
-  import { page } from "$app/stores";
+  import { page } from "$app/state";
+  import Header from "$lib/components/Header";
 
   $: {
-    const currentLocale = $page.data.locale;
+    const currentLocale = page.data.locale;
     if (currentLocale) {
       locale.set(currentLocale);
     }
@@ -13,5 +14,6 @@
 {#if $isLoading}
   <div class="grid place-items-center h-screen">Loading...</div>
 {:else}
+  <Header />
   <slot />
 {/if}
