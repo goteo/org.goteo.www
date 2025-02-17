6462fd3241b3fbd02aac2ba153ccf44637d34e5b
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-02-17 12:02:33 +0100

    chore: update openapi-ts config

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from '@hey-api/openapi-ts';
+import { defaultPlugins } from '@hey-api/openapi-ts';
 
 export default defineConfig({
   input: 'https://v4.goteo.org/v4/docs.json',
   output: 'src/client',
-  plugins: ['@hey-api/client-fetch'],
+  plugins: [
+    ...defaultPlugins,
+    {
+      name: 'zod',
+    },
+    {
+      name: '@hey-api/client-fetch',
+      runtimeConfigPath: './src/api.ts'
+    },
+    {
+      name: '@hey-api/sdk',
+      validator: true
+    }
+  ],
 });
