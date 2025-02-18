cf003e07ec9945ec317955e01549e1d0bd4ec252
@milesibastos <antonio@milesibastos.com>
2025-02-18 07:42:04 -0300

    feat: add Tabs component with content and trigger elements for improved navigation

--- src/routes/[[locale]]/projects/[projectId]/+page.svelte
+++ src/routes/[[locale]]/projects/[projectId]/+page.svelte
@@ -9,6 +9,7 @@
   import LocaleSwitcher from "$lib/components/LocaleSwitcher/LocaleSwitcher.svelte";
   import Player from "$lib/components/Player";
   import RewardCard from "$lib/components/RewardCard";
+  import * as Tabs from "$lib/components/ui/tabs";
 
   let { data }: PageProps = $props();
   let { locales, campaign, video, rewards } = data;
@@ -52,14 +53,29 @@
   </div>
 </section>
 
-<section class="bg-secondary p-8">
-  <div class="flex justify-between items-center mb-8">
-    <h2 class="text-4xl font-bold text-primary-foreground">Selecciona tu recompensa</h2>
-  </div>
+<Tabs.Root value="project" class="">
+  <Tabs.List>
+    <Tabs.Trigger value="project">Información de campaña</Tabs.Trigger>
+    <Tabs.Trigger value="budget">Necesidades</Tabs.Trigger>
+    <Tabs.Trigger value="rewards">Recompensas</Tabs.Trigger>
+    <Tabs.Trigger value="updates">Actualizaciones</Tabs.Trigger>
+    <Tabs.Trigger value="community">Comunidad</Tabs.Trigger>
+  </Tabs.List>
+  <Tabs.Content value="project"></Tabs.Content>
+  <Tabs.Content value="budget">Change your password here.</Tabs.Content>
+  <Tabs.Content value="rewards">
+    <section class="bg-secondary p-8">
+      <div class="flex justify-between items-center mb-8">
+        <h2 class="text-4xl font-bold text-primary-foreground">Selecciona tu recompensa</h2>
+      </div>
 
-  <div class="grid md:grid-cols-3 gap-6">
-    {#each rewards as reward}
-      <RewardCard size="lg" {...reward} />
-    {/each}
-  </div>
-</section>
+      <div class="grid md:grid-cols-3 gap-6">
+        {#each rewards as reward}
+          <RewardCard size="lg" {...reward} />
+        {/each}
+      </div>
+    </section>
+  </Tabs.Content>
+  <Tabs.Content value="updates">Change your password here.</Tabs.Content>
+  <Tabs.Content value="community">Change your password here.</Tabs.Content>
+</Tabs.Root>
