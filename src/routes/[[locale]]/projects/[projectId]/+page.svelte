5347cd173b9f40c9af8e304ec7f416ac41715e7e
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-02-19 13:29:33 +0100

    feat: update project page with hey-api generated api client

--- src/routes/[[locale]]/projects/[projectId]/+page.svelte
+++ src/routes/[[locale]]/projects/[projectId]/+page.svelte
@@ -9,21 +9,18 @@
   import LocaleSwitcher from "$lib/components/LocaleSwitcher/LocaleSwitcher.svelte";
 
   let { data }: PageProps = $props();
-  let { project } = data;
+  let { project, extra } = data;
 </script>
 
 <section class="flex flex-col gap-8">
   <div class="grid grid-flow-col gap-8">
     <div class="space-y-4">
       <p class="text-gray-600 text-2xl">Campaña de crowfunding impulsada por Climática</p>
-      <h1 class="text-5xl font-bold">Apoya la cooperativa de información Climática</h1>
-      <p class="text-gray-600 max-w-3xl">
-        Si crees que los medios deben informar sobre la crisis climática señalando a los culpables en lugar de lavar su
-        imagen, apoya a Climática
-      </p>
+      <h1 class="text-5xl font-bold">{ project.title }</h1>
+      <p class="text-gray-600 max-w-3xl">{project.subtitle}</p>
     </div>
     <div class="flex flex-col items-end justify-between">
-      <LocaleSwitcher locales={project.locales} />
+      <LocaleSwitcher locales={extra.locales} />
       <div class="flex items-center gap-2 text-2xl text-primary-foreground font-medium">
         <Clock size={32} />
         <span>Quedan 16d 23h 57m</span>
@@ -35,11 +32,11 @@
     <div class="bg-[#F3FF73] rounded-lg aspect-video relative">
       <div class="absolute inset-0 flex items-center justify-center">
         <div class="w-16 h-16 bg-white/80 rounded-full flex items-center justify-center">
-          <div class="w-0 h-0 border-l-[20px] border-l-gray-800 border-y-[12px] border-y-transparent ml-1" />
+          <div class="w-0 h-0 border-l-[20px] border-l-gray-800 border-y-[12px] border-y-transparent ml-1" ></div>
         </div>
       </div>
     </div>
-    <CampaignProgress {...project} />
+    <CampaignProgress {...extra} />
   </div>
 </section>
 
--- src/routes/[[locale]]/projects/[projectId]/+page.ts
+++ src/routes/[[locale]]/projects/[projectId]/+page.ts
@@ -1,33 +1,116 @@
-import { z } from "zod";
 import type { PageLoad } from "./$types";
+import { apiAccountingBalancePointsGetCollection, apiProjectsIdGet, type Accounting, type AccountingBalancePoint, type Project } from "../../../../client";
+import { client } from "../../../../client/client.gen";
+import { locales } from "$lib/i18n";
 
-const ProjectSchema = z.object({
-  obtained: z.number(),
-  optimum: z.number(),
-  donations: z.number(),
-  minimum: z.number(),
-  timeSeriesData: z.array(
-    z.object({
-      date: z.coerce.date(),
-      amount: z.number(),
-    })
-  ),
-  locales: z.array(
-    z.object({
-      code: z.string(),
-      label: z.string(),
-    })
-  ),
-});
+const getProject = async (projectId: string): Promise<Project> => {
+  const { data, error } = await apiProjectsIdGet({ path: { id: projectId } });
 
-export const load: PageLoad = async ({ fetch, params }) => {
-  const res = await fetch(`/projects/${params.projectId}`);
-  if (!res.ok) throw new Error("Failed to fetch project data");
+  if (error || typeof data === "undefined") {
+    throw new Error("Failed to fetch project data");
+  }
 
-  const json = await res.json();
-  const parsed = ProjectSchema.safeParse(json);
-  if (!parsed.success) throw new Error("Failed to parse project data");
+  return data;
+}
 
-  const { data: project } = parsed;
-  return { project };
+const getAccounting = async (project: Project): Promise<Accounting> => {
+  if (typeof project.accounting === "undefined") {
+    throw new Error("Project does not have an accounting");
+  }
+
+  const { data, error } = await client.get<Accounting>({
+    url: client.buildUrl({ url: project.accounting })
+  });
+
+  if (error || typeof data === "undefined") {
+    throw new Error("Failed to fetch project accounting");
+  }
+
+  return data;
+}
+
+interface TransactionsData {
+  totalItems: number
+}
+
+const getTransactions = async (project: Project): Promise<TransactionsData> => {
+  if (typeof project.accounting === "undefined") {
+    throw new Error("Project does not have an accounting");
+  }
+
+  const url = client.buildUrl({
+    url: '/v4/accounting_transactions',
+    query: { target: project.accounting }
+  });
+
+  const { data, error } = await client.get<TransactionsData>({
+    headers: { 'Accept': 'application/ld+json' },
+    url: url
+  });
+
+  if (error || typeof data === "undefined") {
+    throw new Error("Failed to fetch project transactions");
+  }
+
+  return data;
+}
+
+const getBalancePoints = async (project: Project): Promise<AccountingBalancePoint[]> => {
+  if (typeof project.accounting === "undefined") {
+    throw new Error("Project does not have an accounting");
+  }
+
+  // TO-DO: MODIFY TO PROJECT'S START DATE
+  const start = new Date();
+  start.setDate(start.getDate() - 1);
+
+  const { data, error } = await apiAccountingBalancePointsGetCollection({
+    query: {
+      accounting: project.accounting,
+      start: start.toISOString()
+    }
+  });
+
+  if (error || typeof data === "undefined") {
+    throw new Error("Failed to fetch project accounting balance data");
+  }
+  
+  return data;
+}
+
+const map = (
+  project: Project,
+  accounting: Accounting,
+  transactions: TransactionsData,
+  balancePoints: Array<AccountingBalancePoint>
+) => {
+  const minimum = Object.values(project.budget?.minimum ?? {}).reduce((acc, { amount }) => acc + amount, 0);
+  const optimum = Object.values(project.budget?.optimum ?? {}).reduce((acc, { amount }) => acc + amount, 0);
+  const obtained = accounting.balance?.amount ?? 0;
+  const donations = transactions.totalItems;
+  const timeSeriesData = balancePoints.map(({ start, balance }) => ({ date: new Date(start ?? ''), amount: balance?.amount ?? 0 }));
+  const projectLocales: Array<{ code: string; label: string }> = project.locales?.map((code) => ({
+    code,
+    ...locales[code],
+  })) ?? [];
+
+  return {
+    minimum,
+    optimum,
+    obtained,
+    donations,
+    timeSeriesData,
+    locales: projectLocales,
+  };
+};
+
+export const load: PageLoad = async ({ params }) => {
+  const project = await getProject(params.projectId);
+  const accounting = await getAccounting(project);
+  const transactions = await getTransactions(project);
+  const balancePoints = await getBalancePoints(project);
+
+  const extra = map(project, accounting, transactions, balancePoints);
+
+  return { project, extra };
 };
