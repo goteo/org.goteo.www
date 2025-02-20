766fbfa772127ad897e56983ca70a7bf7d4011ad
@milesibastos <antonio@milesibastos.com>
2025-02-20 08:00:43 -0300

    feat: implement custom Input component and enhance search input in Navigation

--- src/lib/components/Navigation/Navigation.svelte
+++ src/lib/components/Navigation/Navigation.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+  import { Input } from "$lib/components/ui/input";
   import { Button } from "$lib/components/ui/button";
+  import { Search } from "lucide-svelte";
 </script>
 
 <nav class="border-b px-10 py-6">
   <div class="mx-auto flex gap-6 overflow-x-auto">
-    <input type="search" placeholder="Busca tu campaña" class="px-4 py-2 bg-gray-100 rounded-full text-sm" />
+    <div class="relative max-w-52 w-full">
+      <Search class="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+      <Input type="search" placeholder="Busca tu campaña" class="pl-9 bg-secondary rounded-2xl" />
+    </div>
     <Button variant="ghost" class="whitespace-nowrap">Solidario</Button>
   </div>
 </nav>
