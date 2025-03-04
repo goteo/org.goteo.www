c7aa1fcf2d88b3035a238d79ffceebb7bd50f44d
@milesibastos <antonio@milesibastos.com>
2025-03-04 08:09:53 -0300

    feat: refactor registration form to use IndividualRegisterForm component for individual user details

--- src/routes/[[locale]]/register/+page.svelte
+++ src/routes/[[locale]]/register/+page.svelte
@@ -5,13 +5,13 @@
   import { browser } from "$app/environment";
 
   import * as Form from "$lib/components/ui/form";
-  import { Input } from "$lib/components/ui/input";
   import { Button } from "$lib/components/ui/button";
   import { Checkbox } from "$lib/components/ui/checkbox";
   import * as RadioGroup from "$lib/components/ui/radio-group";
 
   import CheckoutSummary from "$lib/components/CheckoutSummary";
   import { schema } from "./schema";
+  import IndividualRegisterForm from "./IndividualRegisterForm.svelte";
 
   let { data } = $props();
 
@@ -69,50 +69,9 @@
           </RadioGroup.Root>
           <Form.FieldErrors />
         </Form.Fieldset>
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <Form.Field {form} name="first_name">
-            <Form.Control let:attrs>
-              <Input {...attrs} bind:value={$formData.first_name} placeholder="Nombre" />
-            </Form.Control>
-            <Form.FieldErrors />
-          </Form.Field>
-          <Form.Field {form} name="last_name">
-            <Form.Control let:attrs>
-              <Input {...attrs} bind:value={$formData.last_name} placeholder="Apellidos" />
-            </Form.Control>
-            <Form.FieldErrors />
-          </Form.Field>
-        </div>
-        <Form.Field {form} name="email">
-          <Form.Control let:attrs>
-            <Input {...attrs} bind:value={$formData.email} placeholder="Correo electrónico" />
-          </Form.Control>
-          <Form.FieldErrors />
-        </Form.Field>
-        <Form.Field {form} name="password">
-          <Form.Control let:attrs>
-            <Input {...attrs} bind:value={$formData.password} placeholder="Contraseña" />
-          </Form.Control>
-          <Form.FieldErrors />
-        </Form.Field>
-        <Form.Field {form} name="hasTaxId" class="flex items-center space-x-2">
-          <Form.Control let:attrs>
-            <Checkbox {...attrs} bind:checked={$formData.hasTaxId} />
-            <Form.Label class="font-normal">
-              Deseo desgravar esta donación. Es necesario indicar el número de DNI para comunicárselo a Hacienda.
-            </Form.Label>
-            <input name={attrs.name} value={$formData.hasTaxId} hidden />
-          </Form.Control>
-          <Form.FieldErrors />
-        </Form.Field>
 
-        {#if $formData.hasTaxId}
-          <Form.Field {form} name="taxId">
-            <Form.Control let:attrs>
-              <Input {...attrs} bind:value={$formData.taxId} placeholder="DNI*" />
-            </Form.Control>
-            <Form.FieldErrors />
-          </Form.Field>
+        {#if $formData.type === "individual"}
+          <IndividualRegisterForm {form} />
         {/if}
 
         <Form.Field {form} name="terms" class="flex items-center space-x-2">
---
+++ src/routes/[[locale]]/register/IndividualRegisterForm.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import * as Form from "$lib/components/ui/form";
+  import { Input } from "$lib/components/ui/input";
+  import { Checkbox } from "$lib/components/ui/checkbox";
+  import type { SuperForm } from "sveltekit-superforms";
+  import type { FormSchema } from "./schema";
+  
+  export let form: SuperForm<FormSchema>;
+  
+  const { form: formData } = form;
+</script>
+
+<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+  <Form.Field {form} name="first_name">
+    <Form.Control let:attrs>
+      <Input {...attrs} bind:value={$formData.first_name} placeholder="Nombre" />
+    </Form.Control>
+    <Form.FieldErrors />
+  </Form.Field>
+  <Form.Field {form} name="last_name">
+    <Form.Control let:attrs>
+      <Input {...attrs} bind:value={$formData.last_name} placeholder="Apellidos" />
+    </Form.Control>
+    <Form.FieldErrors />
+  </Form.Field>
+</div>
+<Form.Field {form} name="email">
+  <Form.Control let:attrs>
+    <Input {...attrs} bind:value={$formData.email} placeholder="Correo electrónico" />
+  </Form.Control>
+  <Form.FieldErrors />
+</Form.Field>
+<Form.Field {form} name="password">
+  <Form.Control let:attrs>
+    <Input {...attrs} bind:value={$formData.password} placeholder="Contraseña" />
+  </Form.Control>
+  <Form.FieldErrors />
+</Form.Field>
+<Form.Field {form} name="hasTaxId" class="flex items-center space-x-2">
+  <Form.Control let:attrs>
+    <Checkbox {...attrs} bind:checked={$formData.hasTaxId} />
+    <Form.Label class="font-normal">
+      Deseo desgravar esta donación. Es necesario indicar el número de DNI para comunicárselo a Hacienda.
+    </Form.Label>
+    <input name={attrs.name} value={$formData.hasTaxId} hidden />
+  </Form.Control>
+  <Form.FieldErrors />
+</Form.Field>
+
+{#if $formData.hasTaxId}
+  <Form.Field {form} name="taxId">
+    <Form.Control let:attrs>
+      <Input {...attrs} bind:value={$formData.taxId} placeholder="DNI*" />
+    </Form.Control>
+    <Form.FieldErrors />
+  </Form.Field>
+{/if}
