ea06aad1b1714d8cc7d8ff357e936a1412bafb85
jpiere-vailos <jpiere@vailos.com>
2025-03-18 06:50:38 -0500

    refactor(i18n): migrate translations to JSON files

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,27 +1,12 @@
+import es from "./locales/es.json";
+import en from "./locales/en.json";
+
 export const languagesList = {
     es: "Español",
     en: "English",
 };
 
 export const labels = {
-    es: {
-        metadata: {
-            title: "Inicio",
-            description: "Página de inicio",
-        },
-        title: "Hola!",
-        header: {
-            login: "Iniciar sesión",
-        },
-    },
-    en: {
-        metadata: {
-            title: "Home",
-            description: "Home page",
-        },
-        title: "Hello!",
-        header: {
-            login: "Login",
-        },
-    },
+    es,
+    en,
 };
