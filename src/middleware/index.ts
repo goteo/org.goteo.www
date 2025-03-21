0548910f7ae95a06b44f9911e55f737554e04914
jpiere-vailos <jpiere@vailos.com>
2025-03-21 12:48:39 -0500

    refactor: dynamic routes for languages

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,14 +1,27 @@
 import { defineMiddleware } from "astro:middleware";
 
+import { languagesList } from "../i18n/locales/index";
 import { useTranslations } from "../i18n/utils";
 
 import type { Locale } from "../i18n/locales/index";
 import type { APIContext } from "astro";
 
 export const onRequest = defineMiddleware((context: APIContext, next) => {
-    const pathParts = new URL(context.request.url).pathname.split("/");
-    const lang: Locale = pathParts[1] === "en" ? "en" : "es";
+    const { pathname } = new URL(context.request.url);
+    const pathParts = pathname.split("/").filter(Boolean);
 
+    if (pathParts.length < 1) {
+        return context.redirect("/es/", 302);
+    }
+
+    const langSegment = pathParts[0];
+    const validLangs = Object.keys(languagesList);
+
+    if (!validLangs.includes(langSegment)) {
+        return context.redirect("/es/404", 302);
+    }
+
+    const lang: Locale = langSegment as Locale;
     const t = useTranslations(lang);
 
     context.locals.lang = lang;
