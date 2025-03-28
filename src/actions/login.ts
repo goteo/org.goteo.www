90c63df8c29d4a542bf1c8e588e1e7d9b28eb99c
jpiere-vailos <jpiere@vailos.com>
2025-03-28 08:20:12 -0500

    feat: add login

---
+++ src/actions/login.ts
@@ -0,0 +1,50 @@
+import { z } from "astro/zod";
+import { ActionError, defineAction } from "astro:actions";
+
+import { apiUserTokensPost } from "../openapi/client/index.ts";
+
+export const login = defineAction({
+    accept: "form",
+    input: z.object({
+        identifier: z.string(),
+        password: z.string(),
+    }),
+    handler: async (input, context) => {
+        try {
+            const { data } = await apiUserTokensPost({
+                body: {
+                    identifier: input.identifier,
+                    password: input.password,
+                },
+            });
+
+            console.log("data", data);
+
+            if (!data) {
+                throw new ActionError({
+                    code: "BAD_REQUEST",
+                    message: "Correo electrónico o contraseña inválidos.",
+                });
+            }
+
+            context.cookies.set(
+                "access-token",
+                { id: data.id, token: data.token },
+                {
+                    path: "/",
+                    httpOnly: true,
+                    secure: true,
+                    sameSite: "strict",
+                },
+            );
+
+            return { success: true };
+        } catch (error) {
+            console.error("Ocurrió un error inesperado:", error);
+            throw new ActionError({
+                code: "BAD_REQUEST",
+                message: "Ocurrió un error inesperado. Por favor, inténtalo de nuevo más tarde.",
+            });
+        }
+    },
+});
