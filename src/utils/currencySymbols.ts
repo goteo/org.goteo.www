e2f6eb53ff3280a896e2f5a6498a622dd6607423
jpiere-vailos <jpiere@vailos.com>
2025-03-31 09:55:25 -0500

    refactor: redirect

--- src/utils/currencySymbols.ts
+++
@@ -1,8 +0,0 @@
-const currencySymbols: Record<string, string> = {
-    EUR: "€",
-    USD: "$",
-};
-
-export function getCurrencySymbol(currency: string): string {
-    return currencySymbols[currency] ?? currency;
-}
