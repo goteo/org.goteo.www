270831e71467320d146d74fc530ebcae73f7bd17
jpiere-vailos <jpiere@vailos.com>
2025-05-12 01:14:11 -0500

    feat: add details in rows

---
+++ src/components/Admin/DetailsRow.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    import { t } from "../../i18n/store";
+    export let trackingCode: string;
+    export let platformLink: string;
+</script>
+
+<div
+    class="grid grid-cols-4 gap-x-8 gap-y-6 rounded-md bg-[#fbfafe] p-6 text-[16px] text-[#575757]"
+>
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">{$t("contributions.grid.details.operationTime")}</p>
+        <p>-</p>
+    </div>
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">{$t("contributions.grid.details.trackingCode")}</p>
+        <p
+            class="text-tertiary cursor-pointer truncate overflow-hidden whitespace-nowrap underline"
+            title={trackingCode}
+        >
+            {trackingCode}
+        </p>
+    </div>
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">{$t("contributions.grid.details.toWallet")}</p>
+        <p>Sí</p>
+    </div>
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">…</p>
+        <p>…</p>
+    </div>
+
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">{$t("contributions.grid.details.platformLink")}</p>
+        <p
+            class="text-tertiary cursor-pointer truncate overflow-hidden whitespace-nowrap underline"
+            title={platformLink}
+        >
+            {platformLink}
+        </p>
+    </div>
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">{$t("contributions.grid.details.estimatedFee")}</p>
+        <p>-</p>
+    </div>
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">…</p>
+        <p>…</p>
+    </div>
+    <div class="flex flex-col gap-1">
+        <p class="font-semibold">…</p>
+        <p>…</p>
+    </div>
+</div>
