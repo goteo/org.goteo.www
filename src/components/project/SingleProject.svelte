3c9e58d56da93b1d090cf635f6c0eb4b3d14ac31
Jean Piere Rodriguez <jeanpiere.dev@gmail.com>
2025-06-20 01:01:46 -0500

    fix: styles

--- src/components/project/SingleProject.svelte
+++ src/components/project/SingleProject.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-    import Slider from "./../Admin/Slider.svelte";
     import { onMount } from "svelte";
     import type {
         Project,
@@ -10,6 +9,7 @@
     import { getTerritoryTag } from "../../utils/getTerritoryTag";
     import Countdown from "../Countdown.svelte";
     import LanguagesDropdown from "../LanguagesDropdown.svelte";
+    import Sharebutton from "./Sharebutton.svelte";
 
     import { languagesList } from "../../i18n/locales";
     import Tabs from "./Tabs.svelte";
@@ -20,6 +20,7 @@
     import Banner from "./Banner.svelte";
     import { t } from "../../i18n/store";
     import ArrowRightIcon from "../../svgs/ArrowRightIcon.svelte";
+    import RememberIcon from "../../svgs/RememberIcon.svelte";
 
     export let project: Project;
     export let accounting: Accounting;
@@ -39,13 +40,6 @@
     let paragraphRef: HTMLParagraphElement;
     let showToggle = false;
     const countdownEnd = project.calendar?.optimum ? new Date(project.calendar.optimum) : undefined;
-    const slides = [
-        { title: "Total aportes:", amount: "250,95€" },
-        { title: "Total Tips:", amount: "250,96€" },
-        { title: "Total comisiones:", amount: "250,97€" },
-        { title: "Pasar a operativa:", amount: "250,98€" },
-        { title: "Slide 5", amount: "250,99€" },
-    ];
 
     onMount(() => {
         const twoLinesHeight = parseFloat(getComputedStyle(paragraphRef).lineHeight) * 2;
@@ -56,77 +50,91 @@
 </script>
 
 <section class="wrapper">
-    <div class="my-10 flex w-full flex-row justify-between gap-5">
-        <div class="flex w-4/5 flex-col gap-2.5">
-            <div class="flex flex-col gap-2">
-                <h3 class="text-2xl/[32px] font-bold text-[#575757]">
-                    {$t("project.owner")}
-                    <span class="text-tertiary text-2xl font-bold underline"> {ownerName}</span>
-                </h3>
-                <h1 class="text-[40px]/[48px] font-bold text-[#575757]">
-                    {project.title} <span>{ownerName}</span>
-                </h1>
-            </div>
+    <div class="flex flex-col gap-6">
+        <div class="my-10 flex w-full flex-row justify-between gap-5">
+            <div class="flex w-4/5 flex-col gap-2.5">
+                <div class="flex flex-col gap-2">
+                    <h3 class="text-2xl/[32px] font-bold text-[#575757]">
+                        {$t("project.owner")}
+                        <span class="text-tertiary text-2xl font-bold underline"> {ownerName}</span>
+                    </h3>
+                    <h1 class="text-[40px]/[48px] font-bold text-[#575757]">
+                        {project.title} <span>{ownerName}</span>
+                    </h1>
+                </div>
 
-            <div>
-                <p
-                    bind:this={paragraphRef}
-                    class="text-[#575757] transition-all duration-300 ease-in-out {showFull
-                        ? ''
-                        : 'line-clamp-2'}"
-                >
-                    {project.subtitle}
-                </p>
-
-                {#if showToggle}
-                    <button
-                        type="button"
-                        class="mt-2 text-sm font-medium text-blue-600 hover:underline"
-                        on:click={() => (showFull = !showFull)}
+                <div>
+                    <p
+                        bind:this={paragraphRef}
+                        class="text-[#575757] transition-all duration-300 ease-in-out {showFull
+                            ? ''
+                            : 'line-clamp-2'}"
                     >
-                        {showFull ? "Ver menos" : "Ver más"}
-                    </button>
-                {/if}
+                        {project.subtitle}
+                    </p>
+
+                    {#if showToggle}
+                        <button
+                            type="button"
+                            class="mt-2 text-sm font-medium text-blue-600 hover:underline"
+                            on:click={() => (showFull = !showFull)}
+                        >
+                            {showFull ? "Ver menos" : "Ver más"}
+                        </button>
+                    {/if}
+                </div>
+            </div>
+
+            <div class="flex w-1/5 flex-col justify-between">
+                <LanguagesDropdown {languages} />
+
+                <Countdown {countdownEnd} />
             </div>
         </div>
 
-        <div class="flex w-1/5 flex-col justify-between">
-            <LanguagesDropdown {languages} />
-
-            <Countdown {countdownEnd} />
+        <div class="flex items-stretch justify-between gap-6">
+            <div class="h-full w-[70%]">
+                <Player
+                    src={project.video?.src || ""}
+                    title={project.title || ""}
+                    thumbnails={project.video?.thumbnail || ""}
+                    {poster}
+                />
+            </div>
+            <div class="h-full w-[30%]">
+                <Card {project} {accounting} {balancePoints} />
+            </div>
         </div>
+
+        <div class="mb-12 flex w-full flex-row justify-between">
+            <Tags {tags} />
+            <div class="flex flex-row items-center gap-6">
+                <Sharebutton {project} />
+                <button
+                    class="text-tertiary flex cursor-pointer flex-row items-center gap-2 p-2 font-bold"
+                >
+                    <RememberIcon /> {$t("project.actions.remember")}</button
+                >
+            </div>
+        </div>
+
+        <div class="mb-12 flex flex-col gap-6">
+            <div class="flex items-center justify-between">
+                <h2 class="text-secondary text-2xl font-bold">
+                    {$t("reward.trending")}
+                </h2>
+                <button
+                    class="text-tertiary flex cursor-pointer items-center gap-4 rounded-3xl bg-[#E6E5F7] px-6 py-4 font-bold transition"
+                    ><ArrowRightIcon />{$t("reward.showAll")}</button
+                >
+            </div>
+            <Rewards {project} {limit} />
+        </div>
+
+        <div class="mb-12 flex flex-col">
+            <Banner {ownerName} />
+        </div>
+
+        <Tabs {project} />
     </div>
-
-    <div class="flex items-stretch justify-between gap-6">
-        <div class="h-full w-[70%]">
-            <Player
-                src={project.video?.src || ""}
-                title={project.title || ""}
-                thumbnails={project.video?.thumbnail || ""}
-                {poster}
-            />
-        </div>
-        <div class="h-full w-[30%]">
-            <Card {project} {accounting} {balancePoints} />
-        </div>
-    </div>
-
-    <Tags {tags} />
-
-    <div>
-        <div class="flex items-center justify-between">
-            <h2 class="text-secondary text-2xl font-bold">
-                {$t("reward.trending")}
-            </h2>
-            <button
-                class="text-tertiary flex cursor-pointer items-center gap-4 rounded-3xl bg-[#E6E5F7] px-6 py-4 font-bold transition"
-                ><ArrowRightIcon />{$t("reward.showAll")}</button
-            >
-        </div>
-        <Rewards {project} {limit} />
-    </div>
-
-    <Banner {ownerName} />
-    <Slider {slides} />
-    <Tabs {project} />
 </section>
--- src/components/project/Tabs.svelte
+++ src/components/project/Tabs.svelte
@@ -27,69 +27,71 @@
     });
 </script>
 
-<div class="border-b border-gray-200">
-    <div class="flex space-x-6" role="tablist" aria-label="Project tabs">
-        {#each tabs as tab}
-            <button
-                role="tab"
-                aria-selected={activeTab === tab.id}
-                aria-controls={`tab-${tab.id}`}
-                id={`tab-button-${tab.id}`}
-                class="text-tertiary inline-flex items-center rounded-t-lg px-6 py-2 font-bold transition-colors duration-100 ease-in-out"
-                class:bg-[#E6E5F7]={activeTab === tab.id}
-                onclick={() => selectTab(tab.id)}
-            >
-                {tab.label}
-            </button>
-        {/each}
+<div>
+    <div class="border-b border-gray-200">
+        <div class="flex space-x-6" role="tablist" aria-label="Project tabs">
+            {#each tabs as tab}
+                <button
+                    role="tab"
+                    aria-selected={activeTab === tab.id}
+                    aria-controls={`tab-${tab.id}`}
+                    id={`tab-button-${tab.id}`}
+                    class="text-tertiary inline-flex items-center rounded-t-lg px-6 py-2 font-bold transition-colors duration-100 ease-in-out"
+                    class:bg-[#E6E5F7]={activeTab === tab.id}
+                    onclick={() => selectTab(tab.id)}
+                >
+                    {tab.label}
+                </button>
+            {/each}
+        </div>
     </div>
-</div>
 
-<div class="flex w-full justify-center bg-[#E6E5F7] py-20">
-    {#if activeTab === "rewards"}
-        <div
-            id="tab-rewards"
-            role="tabpanel"
-            aria-labelledby="tab-button-rewards"
-            class="w-full px-10"
-        >
-            <ProjectRewards {project} />
-        </div>
-    {:else if activeTab === "project"}
-        <div
-            id="tab-project"
-            role="tabpanel"
-            aria-labelledby="tab-button-project"
-            class="flex max-w-4xl flex-col items-center gap-4 px-8"
-        >
-            {@html contentDescription}
-        </div>
-    {:else if activeTab === "budget"}
-        <div
-            id="tab-budget"
-            role="tabpanel"
-            aria-labelledby="tab-button-budget"
-            class="w-full px-10"
-        >
-            <ProjectBudget {project} />
-        </div>
-    {:else if activeTab === "updates"}
-        <div
-            id="tab-updates"
-            role="tabpanel"
-            aria-labelledby="tab-button-updates"
-            class="w-full px-10"
-        >
-            <ProjectUpdate {project} />
-        </div>
-    {:else if activeTab === "community"}
-        <div
-            id="tab-community"
-            role="tabpanel"
-            aria-labelledby="tab-button-community"
-            class="w-full"
-        >
-            Contenido de comunidad
-        </div>
-    {/if}
+    <div class="flex w-full justify-center bg-[#E6E5F7] py-20">
+        {#if activeTab === "rewards"}
+            <div
+                id="tab-rewards"
+                role="tabpanel"
+                aria-labelledby="tab-button-rewards"
+                class="w-full px-10"
+            >
+                <ProjectRewards {project} />
+            </div>
+        {:else if activeTab === "project"}
+            <div
+                id="tab-project"
+                role="tabpanel"
+                aria-labelledby="tab-button-project"
+                class="flex max-w-4xl flex-col items-center gap-4 px-8"
+            >
+                {@html contentDescription}
+            </div>
+        {:else if activeTab === "budget"}
+            <div
+                id="tab-budget"
+                role="tabpanel"
+                aria-labelledby="tab-button-budget"
+                class="w-full px-10"
+            >
+                <ProjectBudget {project} />
+            </div>
+        {:else if activeTab === "updates"}
+            <div
+                id="tab-updates"
+                role="tabpanel"
+                aria-labelledby="tab-button-updates"
+                class="w-full px-10"
+            >
+                <ProjectUpdate {project} />
+            </div>
+        {:else if activeTab === "community"}
+            <div
+                id="tab-community"
+                role="tabpanel"
+                aria-labelledby="tab-button-community"
+                class="w-full"
+            >
+                Contenido de comunidad
+            </div>
+        {/if}
+    </div>
 </div>
