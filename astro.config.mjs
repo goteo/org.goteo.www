ccdf54a5ae10ad15520a7bfcc34455c65a5d54b1
Jean Piere Rodriguez <jeanpiere.dev@gmail.com>
2025-06-26 11:12:56 -0500

    fix: format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
-// @ts-check
-
 import cloudflare from "@astrojs/cloudflare";
-import node from "@astrojs/node";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
