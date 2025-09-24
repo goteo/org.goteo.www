b910dcb70fea04ae12735596bbc4d93d0693ef36
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-09-24 14:30:03 +0200

    Add base tag component

---
+++ src/components/Tag.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import { twMerge, type ClassNameValue } from "tailwind-merge";
+
+    const baseStyle = [
+        "w-auto",
+        "rounded-[32px]",
+        "px-[16px]",
+        "py-[8px]",
+        "font-[700]",
+        "hover:bg-[#e6e5f7]",
+        "hover:text-secondary",
+    ];
+
+    const styles = {
+        default: "inset-ring-1 hover:inset-ring-0 inset-ring-[#462949] bg-[#fbfbfb]",
+        active: "bg-[#462949] text-primary",
+        ghost: "",
+    };
+
+    let {
+        type = "default",
+        class: className = "",
+        onclick,
+        children,
+    }: {
+        type?: keyof typeof styles;
+        class?: ClassNameValue;
+        onclick?: (event: MouseEvent) => void;
+        children: Snippet;
+    } = $props();
+</script>
+
+<button class={twMerge(baseStyle.join(" "), styles[type], className)} onclick={(e) => onclick?.(e)}>
+    {@render children()}
+</button>
