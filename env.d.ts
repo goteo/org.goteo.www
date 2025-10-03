0545f46f310006b6077ef0818f82a13fc40267e7
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-10-03 17:37:19 +0200

    Remove unused imports

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,4 @@
 /// <reference types="astro/client" />
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import type { useTranslations } from "./i18n/utils";
-import type { Locale } from "../i18n/locales/index";
 
 declare global {
     namespace App {
