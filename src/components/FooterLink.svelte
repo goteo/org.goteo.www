290e8a1fb3aab42fde6f7500b574b45a7ce2f953
Santiago Nahuel de la Fuente <santiagodelafuente2013@gmail.com>
2025-10-15 16:13:33 -0300

    feat: add FooterLink component

---
+++ src/components/FooterLink.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let href;
+    export let extraClasses = "";
+</script>
+
+<a
+    {href}
+    class="text-purple-tint hover:text-soft-purple focus:ring-soft-purple focus:ring-offset-secondary rounded-sm transition-colors duration-200 focus:ring-2 focus:ring-offset-2 focus:outline-none {extraClasses}"
+>
+    <slot />
+</a>
