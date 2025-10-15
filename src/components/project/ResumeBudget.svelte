a21f9ff9c6ee0a99ab8c0538031afd4f18b69066
Santiago Nahuel de la Fuente <santiagodelafuente2013@gmail.com>
2025-10-15 16:22:19 -0300

    feat: update ResumeBudget component with enhanced UI elements

--- src/components/project/ResumeBudget.svelte
+++ src/components/project/ResumeBudget.svelte
@@ -2,7 +2,6 @@
     import BudgetBar from "./BudgetBar.svelte";
     import LegendBudgetBar from "./LegendBudgetBar.svelte";
     import { t } from "../../i18n/store";
-    import { formatCurrency } from "../../utils/currencies";
     import type { Project, Accounting } from "../../openapi/client/index";
 
     let { project, accounting } = $props<{
@@ -16,36 +15,8 @@
         {$t("project.tabs.budget.chart-title")}
     </div>
 
-    <div class="flex basis-2/3 flex-col gap-8 rounded-xl bg-[#f1efff] p-6">
-        <div class="text-secondary flex flex-col gap-4">
-            <div class="flex flex-row gap-2">
-                {$t("project.tabs.budget.minimum")}:
-                <span>
-                    <p class="font-bold">
-                        {formatCurrency(
-                            project.budget.minimum.money.amount,
-                            project.budget.minimum.money.currency,
-                        )}
-                    </p>
-                </span>
-            </div>
-            <BudgetBar {project} {accounting} budgetAmount={"minimum"} />
-        </div>
-
-        <div class="text-secondary flex flex-col gap-4">
-            <div class="flex flex-row gap-2">
-                {$t("project.tabs.budget.optimal")}:
-                <span>
-                    <p class="font-bold">
-                        {formatCurrency(
-                            project.budget.optimum.money.amount,
-                            project.budget.optimum.money.currency,
-                        )}
-                    </p>
-                </span>
-            </div>
-            <BudgetBar {project} {accounting} budgetAmount={"optimum"} />
-        </div>
-        <LegendBudgetBar />
+    <div class="bg-divider flex basis-2/3 flex-col gap-8 rounded-xl p-6">
+        <BudgetBar {project} {accounting} />
+        <LegendBudgetBar {project} />
     </div>
 </div>
