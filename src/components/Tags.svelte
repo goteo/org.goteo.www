f4790c01b78d55cc82d8b856410bee5afb7b1161
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-11-07 16:01:39 +0100

    feat: refactor category tag

--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
-    import BookmarkIcon from "../svgs/BookmarkIcon.svelte";
-    import { t } from "../i18n/store";
     import type { Project } from "../openapi/client";
     import TerritoryTag from "./TerritoryTag.svelte";
+    import CategoryTag from "./CategoryTag.svelte";
 
-    export let lang: string;
     export let project: Project;
+
+    function parseCategoryIri(category: string) {
+        return category.split("/")[3];
+    }
 </script>
 
 <div class="flex w-auto gap-2">
-    <div class="flex items-center gap-2 rounded-sm border border-black px-2 py-1">
-        <BookmarkIcon />
-        {$t(`categories.${project.category}`)}
-    </div>
+    {#each project.categories as category}
+        <CategoryTag category={parseCategoryIri(category)} />
+    {/each}
     <TerritoryTag territory={project.territory} />
 </div>
