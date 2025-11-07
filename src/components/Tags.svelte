4a9dced42f29882791fe919bd1613316a3378832
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-11-07 15:22:55 +0100

    feat: extract territory tag handling to own component

--- src/components/Tags.svelte
+++ src/components/Tags.svelte
@@ -1,27 +1,11 @@
 <script lang="ts">
     import BookmarkIcon from "../svgs/BookmarkIcon.svelte";
-    import MapIcon from "../svgs/MapIcon.svelte";
     import { t } from "../i18n/store";
-    import type { Project, Territory } from "../openapi/client";
-    import iso3166 from "iso-3166-2";
-    import { getTerritoryTag } from "../utils/getTerritoryTag";
+    import type { Project } from "../openapi/client";
+    import TerritoryTag from "./TerritoryTag.svelte";
 
     export let lang: string;
     export let project: Project;
-
-    function displayName(territory: Territory) {
-        const countryNames = new Intl.DisplayNames(lang, { type: "region" });
-        const country = countryNames.of(territory.country);
-
-        const tag = getTerritoryTag(territory);
-        if (!tag || tag === territory.country) {
-            return country;
-        }
-
-        const iso = iso3166.subdivision(tag!);
-
-        return iso?.name;
-    }
 </script>
 
 <div class="flex w-auto gap-2">
@@ -29,7 +13,5 @@
         <BookmarkIcon />
         {$t(`categories.${project.category}`)}
     </div>
-    <div class="flex items-center gap-2 rounded-sm border border-black px-2 py-1">
-        <MapIcon />{displayName(project.territory)}
-    </div>
+    <TerritoryTag territory={project.territory} />
 </div>
---
+++ src/components/TerritoryTag.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import MapIcon from "../svgs/MapIcon.svelte";
+    import type { Territory } from "../openapi/client";
+    import iso3166 from "iso-3166-2";
+    import { getTerritoryTag } from "../utils/getTerritoryTag";
+    import Tag from "./library/Tag.svelte";
+    import type { Locale } from "../i18n/locales";
+    import { locale } from "../i18n/store";
+
+    let {
+        territory,
+        lang: initialLang = undefined,
+    }: {
+        territory: Territory;
+        lang?: Locale;
+    } = $props();
+
+    const lang = initialLang || $locale;
+
+    function displayName(territory: Territory) {
+        const countryNames = new Intl.DisplayNames(lang, { type: "region" });
+        const country = countryNames.of(territory.country!);
+
+        const tag = getTerritoryTag(territory);
+        if (!tag || tag === territory.country) {
+            return country;
+        }
+
+        const iso = iso3166.subdivision(tag!);
+
+        return `${iso?.name}, ${country}`;
+    }
+</script>
+
+<Tag>
+    <MapIcon />
+    <span>{displayName(territory)}</span>
+</Tag>
