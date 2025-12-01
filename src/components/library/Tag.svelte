3c2b018226b8132f47a5697ab7feae0cedc9a215
Daniel Subiabre <daniel.subiabre@platoniq.net>
2025-12-01 13:40:33 +0100

    feat: updated Tag component to have bold (bg+icon) variant style

--- src/components/library/Tag.svelte
+++ src/components/library/Tag.svelte
@@ -3,25 +3,25 @@
     import { twMerge, type ClassNameValue } from "tailwind-merge";
 
     interface Props {
-        variant?: keyof typeof variantStyles;
+        variant?: keyof typeof variantStyles | undefined;
         class?: ClassNameValue;
         children: Snippet;
     }
 
-    let { variant = "default", class: classes = "", children }: Props = $props();
+    let { variant, class: classes = "", children }: Props = $props();
 
     const variantStyles = {
-        default: "bg-white border border-black text-secondary",
         success: "bg-variant2",
         warning: "bg-variant4",
         error: "bg-tertiary",
+        bold: "bg-grey border-grey text-secondary font-bold",
     };
 </script>
 
 <div
     class={twMerge(
-        "flex content-center items-center gap-2 rounded-[4px] px-2 py-1 whitespace-nowrap text-black",
-        variantStyles[variant],
+        "flex content-center items-center gap-2 rounded-[4px] border border-black bg-white px-2 py-1 whitespace-nowrap text-black",
+        variant ? variantStyles[variant] : "",
         classes,
     )}
 >
