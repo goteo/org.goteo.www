f337b94f7ff1dc68be45208506c3f37ee0007051
Dayv1dDev <david.moreno@platoniq.net>
2025-12-02 17:39:50 +0100

    refactor: remove unused deviceWidth state and resize effect from Toast component

--- src/components/library/Toast.svelte
+++ src/components/library/Toast.svelte
@@ -4,7 +4,6 @@
     import SuccessIcon from "../../svgs/SuccessIcon.svelte";
     import NotificationIcon from "../../svgs/NotificationIcon.svelte";
     import WarningIcon from "../../svgs/WarningIcon.svelte";
-    import { innerWidth } from "svelte/reactivity/window";
 
     let {
         variant = "error",
@@ -21,7 +20,6 @@
     } = $props();
 
     let showToast = $state(true);
-    let deviceWidth = $state(innerWidth);
 
     const variantStyles = {
         error: "border-tertiary bg-semantic-error",
@@ -29,65 +27,60 @@
         notification: "border-variant3 bg-semantic-notification",
         warning: "border-variant4 bg-semantic-warning",
     };
-
-    $effect(() => {
-        const handleResize = () => {
-            deviceWidth = innerWidth;
-        };
-
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    });
 </script>
 
 <div
     class={twMerge(
-        `flex min-w-[327px] items-center justify-between rounded-lg border p-6 @max-sm:gap-4`,
+        `min-w-[327px] items-center gap-4 rounded-lg border p-6 sm:justify-between mx-4`,
         classes,
         variantStyles[variant],
-        showToast ? "opacity-100" : "hidden opacity-0",
+        showToast ? "flex opacity-100" : "hidden opacity-0",
     )}
 >
-    {#if deviceWidth > 640}
-        <div class="flex items-center justify-center gap-4">
-            {#if variantStyles[variant] === variantStyles.error}
-                <ErrorIcon class="size-6" />
-            {:else if variantStyles[variant] === variantStyles.success}
-                <SuccessIcon class="size-6" />
-            {:else if variantStyles[variant] === variantStyles.notification}
-                <NotificationIcon class="size-6" />
-            {:else if variantStyles[variant] === variantStyles.warning}
-                <WarningIcon className="size-6" />
-            {/if}
-            {#if children}
-                <div class="text-center">
-                    {@render children()}
-                </div>
-            {/if}
-        </div>
-        <div class="flex items-center gap-4">
+    <div class="flex items-center justify-center gap-4">
+        {#if variantStyles[variant] === variantStyles.error}
+            <ErrorIcon class="size-6 shrink-0" />
+        {:else if variantStyles[variant] === variantStyles.success}
+            <SuccessIcon class="size-6 shrink-0" />
+        {:else if variantStyles[variant] === variantStyles.notification}
+            <NotificationIcon class="size-6 shrink-0" />
+        {:else if variantStyles[variant] === variantStyles.warning}
+            <WarningIcon className="size-6 shrink-0" />
+        {/if}
+        {#if children}
+            <div class="">
+                {@render children()}
+            </div>
+        {/if}
+    </div>
+    <div class="">
+        {#if button || link}
+            <div class="hidden sm:flex">
+                {@render button?.()}
+                {@render link?.()}
+            </div>
+        {/if}
+        <button onclick={() => (showToast = false)} aria-label="Close Toast" class="p-1">
+            <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M0.21967 0.21967C0.512563 -0.0732233 0.987437 -0.0732233 1.28033 0.21967L7.5 6.43934L13.7197 0.21967C14.0126 -0.0732233 14.4874 -0.0732233 14.7803 0.21967C15.0732 0.512563 15.0732 0.987437 14.7803 1.28033L8.56066 7.5L14.7803 13.7197C15.0732 14.0126 15.0732 14.4874 14.7803 14.7803C14.4874 15.0732 14.0126 15.0732 13.7197 14.7803L7.5 8.56066L1.28033 14.7803C0.987437 15.0732 0.512563 15.0732 0.21967 14.7803C-0.0732233 14.4874 -0.0732233 14.0126 0.21967 13.7197L6.43934 7.5L0.21967 1.28033C-0.0732233 0.987437 -0.0732233 0.512563 0.21967 0.21967Z"
+                    fill="#462949"
+                />
+            </svg>
+        </button>
+    </div>
+    {#if button || link}
+        <div class="mt-4 flex items-center justify-center sm:hidden">
             {@render button?.()}
             {@render link?.()}
-            <button onclick={() => (showToast = false)} aria-label="Close Toast" class="p-1">
-                <svg
-                    width="15"
-                    height="15"
-                    viewBox="0 0 15 15"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.21967 0.21967C0.512563 -0.0732233 0.987437 -0.0732233 1.28033 0.21967L7.5 6.43934L13.7197 0.21967C14.0126 -0.0732233 14.4874 -0.0732233 14.7803 0.21967C15.0732 0.512563 15.0732 0.987437 14.7803 1.28033L8.56066 7.5L14.7803 13.7197C15.0732 14.0126 15.0732 14.4874 14.7803 14.7803C14.4874 15.0732 14.0126 15.0732 13.7197 14.7803L7.5 8.56066L1.28033 14.7803C0.987437 15.0732 0.512563 15.0732 0.21967 14.7803C-0.0732233 14.4874 -0.0732233 14.0126 0.21967 13.7197L6.43934 7.5L0.21967 1.28033C-0.0732233 0.987437 -0.0732233 0.512563 0.21967 0.21967Z"
-                        fill="#462949"
-                    />
-                </svg>
-            </button>
         </div>
-    {:else}
-        
     {/if}
 </div>
