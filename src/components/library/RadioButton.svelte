4dd7e942984d6c1e163ec281b482c9cd0d198c01
Dayv1dDev <david.moreno@platoniq.net>
2025-12-04 13:50:55 +0100

    fix: update RadioButton styles to match Figma

--- src/components/library/RadioButton.svelte
+++ src/components/library/RadioButton.svelte
@@ -8,7 +8,14 @@
         children?: any;
     }
 
-    let { group = $bindable(), value, label, class: className, children = null, ...rest }: Props = $props();
+    let {
+        group = $bindable(),
+        value,
+        label,
+        class: className,
+        children = null,
+        ...rest
+    }: Props = $props();
 </script>
 
 <label class="flex cursor-pointer items-center gap-2 {className}">
@@ -17,7 +24,7 @@
             type="radio"
             bind:group
             {value}
-            class="checked:bg-primary checked:border-primary checked:text-primary focus:outline-0 focus:shadow-none border-secondary relative h-6 w-6 appearance-none rounded-full border after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:bg-purple-900 after:content-[''] checked:after:block"
+            class="checked:bg-primary checked:border-primary checked:text-primary border-secondary after:bg-secondary relative h-6 w-6 appearance-none rounded-full border bg-white after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:content-[''] checked:after:block focus:shadow-none focus:outline-0"
             {...rest}
         />
     </div>
