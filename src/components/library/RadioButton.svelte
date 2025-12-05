5430a60ec029573a5afd49b2596bab054cb04c3b
Dayv1dDev <david.moreno@platoniq.net>
2025-12-05 16:57:53 +0100

    fix: change var "className" name to "classes", move twMerge to input and refactor conditional rendering for "label" var

--- src/components/library/RadioButton.svelte
+++ src/components/library/RadioButton.svelte
@@ -9,29 +9,25 @@
         children?: any;
     }
 
-    let {
-        group = $bindable(),
-        label,
-        class: className,
-        children = null,
-        ...rest
-    }: Props = $props();
+    let { group = $bindable(), label, class: classes, children = null, ...rest }: Props = $props();
 </script>
 
-<label class={twMerge("flex cursor-pointer items-center gap-2", className)}>
+<label class="flex cursor-pointer items-center gap-2">
     <div class="w-6">
         <input
             type="radio"
             bind:group
-            class="checked:bg-primary checked:border-primary checked:text-primary border-secondary after:bg-secondary relative h-6 w-6 appearance-none rounded-full border bg-white after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:content-[''] checked:after:block focus:shadow-none focus:outline-0"
+            class={twMerge(
+                "checked:bg-primary checked:border-primary checked:text-primary border-secondary after:bg-secondary relative h-6 w-6 appearance-none rounded-full border bg-white after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:content-[''] checked:after:block focus:shadow-none focus:outline-0",
+                classes,
+            )}
             {...rest}
         />
     </div>
-    <span class="text-gray-700">
-        {#if label}
+    {#if label}
+        <span class="text-gray-700">
             {label}
-        {:else}
-            {@render children()}
-        {/if}
-    </span>
+        </span>
+    {/if}
+    {@render children()}
 </label>
