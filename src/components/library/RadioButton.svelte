9ae16a12d2f508c8c748b2877e4c9627f5785a12
Dayv1dDev <david.moreno@platoniq.net>
2025-12-05 16:45:23 +0100

    fix: remove value prop as it's already declared in the input attributes type

--- src/components/library/RadioButton.svelte
+++ src/components/library/RadioButton.svelte
@@ -4,7 +4,6 @@
 
     interface Props extends HTMLInputAttributes {
         group?: any;
-        value?: any;
         label?: string;
         class?: string;
         children?: any;
@@ -12,7 +11,6 @@
 
     let {
         group = $bindable(),
-        value,
         label,
         class: className,
         children = null,
@@ -25,7 +23,6 @@
         <input
             type="radio"
             bind:group
-            {value}
             class="checked:bg-primary checked:border-primary checked:text-primary border-secondary after:bg-secondary relative h-6 w-6 appearance-none rounded-full border bg-white after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:content-[''] checked:after:block focus:shadow-none focus:outline-0"
             {...rest}
         />
