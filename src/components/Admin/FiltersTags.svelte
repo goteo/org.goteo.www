6a22fe7e9f31229db6a5aa13a6034bb206a17533
Dayv1dDev <david.moreno@platoniq.net>
2025-12-11 20:02:26 +0100

    feat: implement tags filter to get only the filters that had been used

--- src/components/Admin/FiltersTags.svelte
+++ src/components/Admin/FiltersTags.svelte
@@ -1,18 +1,33 @@
 <script lang="ts">
     import CloseIcon from "../../svgs/CloseIcon.svelte";
     import Tag from "../library/Tag.svelte";
+    import { t } from "../../i18n/store";
 
     let { title, filters } = $props<{
         title: string;
         filters?: any;
     }>();
 
-    let tags: { title: string; value: string }[] | undefined = $state(undefined);
+    let tags:
+        | { title: string; value?: string; values?: { from: string; to: string } }[]
+        | undefined = $state(undefined);
+
+    function closeTag() {
+        
+    }
 
     $effect(() => {
-        tags = Object.keys(filters).map((filter: string) => {
-            return { title: filter, value: filters[filter] };
-        });
+        tags = Object.keys(filters)
+            .map((filter: string) => {
+                if (filter === "date") return { title: filter, values: { ...filters[filter] } };
+                else return { title: filter, value: filters[filter] };
+            })
+            .filter((filter) => {
+                if (filter.title === "date")
+                    return filter.values.from !== undefined || filter.values.to !== undefined;
+                else if (filter.title === "target") return filter.value !== undefined;
+                else return filter.value !== "";
+            });
     });
 </script>
 
@@ -20,12 +35,17 @@
     <h1 class="text-2xl/8 font-bold text-black">
         {title}
     </h1>
-    {#if tags}
-        {#each tags as tag}
-            <Tag variant={"bold"}>
-                {tag}
+
+    {#each tags as tag}
+        <Tag variant={"bold"}>
+            {#if tag.title === "date"}
+                {`${tag.values?.from} - ${tag.values?.to}`}
+            {:else}
+                {tag.value}
+            {/if}
+            <button onclick={closeTag} class="size-auto">
                 <CloseIcon class="size-[15px]" />
-            </Tag>
-        {/each}
-    {/if}
+            </button>
+        </Tag>
+    {/each}
 </div>
