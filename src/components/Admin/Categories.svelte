25454b99e80bd5846ea4e1bdfaf74f4ec6ad71e7
Dayv1dDev <david.moreno@platoniq.net>
2025-12-18 20:35:48 +0100

    feat: start implementing Categories component

---
+++ src/components/Admin/Categories.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Category from "../library/Category.svelte";
+
+    let categories = $state([]);
+</script>
+
+<div>
+    {#each categories as category}
+        <Category>a</Category>
+    {/each}
+</div>
