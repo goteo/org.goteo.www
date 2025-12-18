a91788fba7d50113346d1413aec4070609994f6c
Dayv1dDev <david.moreno@platoniq.net>
2025-12-18 14:32:51 +0100

    feat: add missing styles and change the icon to the proper one from Figma in ExportCsv

--- src/components/Admin/ExportCsv.svelte
+++ src/components/Admin/ExportCsv.svelte
@@ -179,38 +179,28 @@
                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
         </svg>
-        {$t("contributions.export.exporting")}
+        <span class="text-secondary font-bold">
+            {$t("contributions.export.exporting")}
+        </span>
     {:else}
         <svg
+            class="text-secondary"
+            xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
-            viewBox="0 0 24 24"
+            viewBox="0 0 16 16"
             fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            class="text-gray-600"
         >
             <path
-                d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            />
-            <path
-                d="M7 10L12 15L17 10"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-            />
-            <path
-                d="M12 15V3"
-                stroke="currentColor"
-                stroke-width="2"
+                d="M2 14H14M5 8.6L8 11.6L11 8.6M8 2V10.4"
+                stroke="#462949"
+                stroke-width="1.33333"
                 stroke-linecap="round"
                 stroke-linejoin="round"
             />
         </svg>
-        {$t("contributions.export.csv")}
+        <span class="text-secondary font-bold">
+            {$t("contributions.export.csv")}
+        </span>
     {/if}
 </button>
