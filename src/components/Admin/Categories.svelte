b6b43d786380a06c71c2400dab4828f844543e87
Dayv1dDev <david.moreno@platoniq.net>
2025-12-19 12:10:51 +0100

    feat: add all styles to Categories component

--- src/components/Admin/Categories.svelte
+++ src/components/Admin/Categories.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
+    import { onMount } from "svelte";
     import Category from "../library/Category.svelte";
+    import { formatCurrency } from "../../utils/currencies";
 
-    let categories = $state([]);
+    let categories: { title: string; value: string }[] = $state([]);
+
+    onMount(async () => {
+        // Fetch categories from an API or state management
+        categories = [
+            { title: "Technology:", value: "42" },
+            { title: "Health:", value: "27" },
+            { title: "Science:", value: "15000000" },
+            { title: "Art:", value: "8" },
+        ];
+    });
 </script>
 
-<div>
+<div class="flex flex-wrap gap-[8px]">
     {#each categories as category}
-        <Category>a</Category>
+        <Category class="text-secondary hover:bg-white hover:inset-ring-1 hover:inset-ring-secondary">
+            {category.title} {formatCurrency(Number(category.value), "USD")}
+        </Category>
     {/each}
 </div>
