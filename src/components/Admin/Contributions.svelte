b5ab7e247ad930c098c51e2943767d954e850987
Dayv1dDev <david.moreno@platoniq.net>
2026-01-19 13:33:52 +0100

    refactor: move filters options effects to Contributions component to use them for categories array too

--- src/components/Admin/Contributions.svelte
+++ src/components/Admin/Contributions.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-    import Slider from "./Slider.svelte";
-    import Filters from "./Filters.svelte";
-    import Table from "./Table.svelte";
-    import Categories from "./Categories.svelte";
-    import FiltersTags from "./FiltersTags.svelte";
+    import {
+        apiGatewaysGetCollection,
+        type ApiGatewayChargesGetCollectionData,
+    } from "../../openapi/client";
+    import { onMount } from "svelte";
     import { t } from "../../i18n/store";
+    import Categories from "./Categories.svelte";
     import ExportCsv from "./ExportCsv.svelte";
-    import type { ApiGatewayChargesGetCollectionData } from "../../openapi/client";
-    
+    import Filters from "./Filters.svelte";
+    import FiltersTags from "./FiltersTags.svelte";
+    import Slider from "./Slider.svelte";
+    import Table from "./Table.svelte";
+
     const slides = [
         { title: $t("admin.projects.totalizers.selectedCampaigns"), amount: "432" },
         { title: $t("admin.charges.totalizers.totalCharges"), amount: "250,98€" },
@@ -23,14 +27,48 @@
     ];
 
     let filters: ApiGatewayChargesGetCollectionData["query"] = $state({});
+    let paymentMethodOptions = $state<[string, string][]>([]);
+    let chargeStatusOptions = $state<[string, string][]>([]);
+    let rangeAmountOptions = $state<[string, string][]>([]);
 
     function handleApplyFilters(newFilters: ApiGatewayChargesGetCollectionData["query"]) {
         filters = { ...filters, ...newFilters };
     }
+
+    onMount(async () => {
+        const { data: paymentGateways } = await apiGatewaysGetCollection();
+
+        paymentMethodOptions = [
+            ["all", $t("contributions.filters.paymentMethod.options.all")],
+            ...(paymentGateways ?? []).map((g): [string, string] => [
+                g.name!,
+                $t(`contributions.filters.paymentMethod.options.${g.name}`),
+            ]),
+        ];
+
+        chargeStatusOptions = Object.entries($t("contributions.filters.chargeStatus.options"));
+        rangeAmountOptions = Object.entries($t("contributions.filters.rangeAmount.options")).sort(
+            ([a], [b]) => {
+                const parseMin = (val: string) =>
+                    val.includes("..") ? parseInt(val.split("..")[0]) : parseInt(val);
+                if (a === "all") return -1;
+                if (b === "all") return 1;
+                return parseMin(a) - parseMin(b);
+            },
+        );
+
+        console.log(paymentMethodOptions);
+    });
 </script>
 
 <div class="flex flex-col gap-10">
-    <Filters {filters} onApplyFilters={handleApplyFilters} />
+    <Filters
+        {filters}
+        {paymentMethodOptions}
+        {chargeStatusOptions}
+        {rangeAmountOptions}
+        onApplyFilters={handleApplyFilters}
+    />
     <div class="flex flex-col">
         <div class="mb-8 flex justify-between">
             <FiltersTags
