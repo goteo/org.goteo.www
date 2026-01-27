ab3a809589d9631500edeaeed906fc82267168c3
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-01-27 14:21:40 +0100

    test: increase cypress timeouts to 3 minutes

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
     e2e: {
         baseUrl: "http://localhost:4321",
-        pageLoadTimeout: 120000,
-        defaultCommandTimeout: 65000,
-        requestTimeout: 65000,
-        responseTimeout: 65000,
+        pageLoadTimeout: 180000,
+        defaultCommandTimeout: 180000,
+        requestTimeout: 180000,
+        responseTimeout: 180000,
         setupNodeEvents(/* on, config */) {
             // implement node event listeners here
         },
