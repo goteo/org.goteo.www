f56e452c36b868e88f19a8b0e21886523fa77e83
Dayv1dDev <david.moreno@platoniq.net>
2026-01-30 13:45:26 +0100

    fix: move checkboxes to library component, starting restyling and props handling; pending to use this component in the rest of checkboxes

---
+++ src/components/library/Checkbox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { id, label } = $props<{
+        id?: string;
+        label?: string;
+    }>();
+</script>
+
+<input
+    type="checkbox"
+    {id}
+    class="text-primary mt-1 size-5 shrink-0 border ring-0 border-secondary rounded-sm"
+/>
+<span class="text-sm text-gray-700">{label}</span>
