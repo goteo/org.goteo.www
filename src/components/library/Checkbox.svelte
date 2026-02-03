ead2ef2d333666631bece8fa833d087f735284bf
Dayv1dDev <david.moreno@platoniq.net>
2026-02-03 09:36:34 +0100

    feat: add binding with checked prop/attr and id prop for label as well

--- src/components/library/Checkbox.svelte
+++ src/components/library/Checkbox.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
-    let { id, label } = $props<{
+    let { id, label, checked = $bindable(false) } = $props<{
         id?: string;
         label?: string;
+        checked?: boolean;
     }>();
 </script>
 
 <input
     type="checkbox"
     {id}
+    bind:checked
     class="text-primary border-secondary mt-1 size-5 shrink-0 rounded-sm border ring-0"
 />
 {#if label}
-    <span class="text-content text-base font-normal">{label}</span>
+    <label for={id} class="text-content text-base font-normal">{label}</label>
 {/if}
