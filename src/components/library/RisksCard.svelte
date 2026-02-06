b850de7cb0c88751f8f04d9725cde99f1a26105c
Dayv1dDev <david.moreno@platoniq.net>
2026-02-06 12:20:38 +0100

    feat: add props (with tag variant change handling), fix some wrong styles, i18n keys with riskType & riskTier props, and start new msg handling in RisksCard component

--- src/components/library/RisksCard.svelte
+++ src/components/library/RisksCard.svelte
@@ -1,24 +1,52 @@
 <script lang="ts">
+    import { t } from "../../i18n/store";
     import Button from "./Button.svelte";
     import Tag from "./Tag.svelte";
+
+    let {
+        riskType,
+        riskTier,
+        newMessage = false,
+    } = $props<{
+        riskType: "config" | "info" | "rewards" | "collabs" | "budget" | "about";
+        riskTier: "low" | "medium" | "high";
+        newMessage?: boolean;
+    }>();
+
+    let newMsgCardShadow =
+        "0 35px 10px 0 rgba(0, 0, 0, 0.00), 0 22px 9px 0 rgba(0, 0, 0, 0.01), 0 13px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 6px 0 rgba(0, 0, 0, 0.09), 0 1px 3px 0 rgba(0, 0, 0, 0.10)";
+    let tagVariant: "success" | "error" | "warning" | "bold" | undefined = $state();
+
+    $effect(() => {
+        if (riskTier === "low") tagVariant = "success";
+        else if (riskTier === "medium") tagVariant = "warning";
+        else if (riskTier === "high") tagVariant = "error";
+    });
 </script>
 
-<article class="flex flex-col p-6 rounded-2xl gap-8 border border-variant1 bg-soft-purple w-full max-w-[437px]">
+<article
+    class="border-variant1 bg-soft-purple flex w-full max-w-[437px] flex-col gap-8 rounded-2xl border p-6 {newMessage
+        ? `shadow-[${newMsgCardShadow}]`
+        : ''}"
+>
     <div class="flex flex-col gap-4">
         <div class="flex justify-between">
             <div class="flex flex-col gap-1">
-                <h2 class="text-secondary text-2xl font-bold">Configuración</h2>
+                <h2 class="text-secondary text-2xl font-bold">{$t(`reviews.risks-cards.types.${riskType}.title`)}</h2>
+                <!-- TODO: Messages reactivity functionality (new messages + future chatbox logic) -->
                 <span class="text-content text-sm/4">32 chats. Última actividad 12/11/2025</span>
             </div>
-            <Tag variant="success">Riesgo bajo</Tag>
+            <Tag variant={tagVariant}>{$t(`reviews.risks-cards.tags.${riskTier}`)}</Tag>
         </div>
-        <p class="text-base text-content w-full min-h-24 max-h-24 overflow-hidden overflow-ellipsis">
-            Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem
-            ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet
+        <p
+            class="text-content max-h-24 min-h-24 w-full overflow-hidden text-base overflow-ellipsis"
+        >
+            {$t(`reviews.risks-cards.types.${riskType}.description.${riskTier}`)}
         </p>
     </div>
     <div class="flex justify-between">
-        <Button kind="ghost">Ver conversación</Button>
-        <Button kind="secondary">Cambiar riesgo</Button>
+        <!-- TODO: Add functionality to both buttons -->
+        <Button kind="ghost">{$t("reviews.btns.seeChat")}</Button>
+        <Button kind="secondary">{$t("reviews.btns.changeRisk")}</Button>
     </div>
 </article>
