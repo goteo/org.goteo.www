724491db66406ec0470795c6ebe396bbf7f8a2d7
Dayv1dDev <david.moreno@platoniq.net>
2026-02-16 14:18:49 +0100

    feat: add Grid component with its corresponding props (children + classes with twMerge)

---
+++ src/components/library/Grid.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { twMerge, type ClassNameValue } from "tailwind-merge";
+
+    let { class: classes = "gap-6 lg:grid-cols-3", children } = $props<{
+        class?: ClassNameValue;
+        children: any;
+    }>();
+</script>
+
+<div class={twMerge("grid", classes)}>
+    {@render children()}
+</div>
