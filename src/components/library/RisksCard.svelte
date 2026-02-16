e0af2f8e668027c297ede52bb1668b4664eb20d1
Dayv1dDev <david.moreno@platoniq.net>
2026-02-16 13:16:12 +0100

    fix: refactor RisksCard component to use SDK ProjectReviewArea type + modify & delete i18n keys

--- src/components/library/RisksCard.svelte
+++ src/components/library/RisksCard.svelte
@@ -2,25 +2,21 @@
     import { t } from "../../i18n/store";
     import Button from "./Button.svelte";
     import Tag from "./Tag.svelte";
+    import type { ProjectReviewArea } from "../../openapi/client";
 
-    let {
-        riskType,
-        riskTier,
-        newMessage = false,
-    } = $props<{
-        riskType: "config" | "info" | "rewards" | "collabs" | "budget" | "about";
-        riskTier: "low" | "medium" | "high";
+    let { review, newMessage = false } = $props<{
+        review: ProjectReviewArea;
         newMessage?: boolean;
     }>();
 
     let newMsgCardShadow =
         "0 35px 10px 0 rgba(0, 0, 0, 0.00), 0 22px 9px 0 rgba(0, 0, 0, 0.01), 0 13px 8px 0 rgba(0, 0, 0, 0.05), 0 6px 6px 0 rgba(0, 0, 0, 0.09), 0 1px 3px 0 rgba(0, 0, 0, 0.10)";
-    let tagVariant: "success" | "error" | "warning" | "bold" | undefined = $state();
+    let tagVariant: any = $state();
 
     $effect(() => {
-        if (riskTier === "low") tagVariant = "success";
-        else if (riskTier === "medium") tagVariant = "warning";
-        else if (riskTier === "high") tagVariant = "error";
+        if (review.risk === "low") tagVariant = "success";
+        else if (review.risk === "medium") tagVariant = "warning";
+        else if (review.risk === "high") tagVariant = "error";
     });
 </script>
 
@@ -35,22 +31,22 @@
                 <!-- TODO: Messages reactivity functionality (new messages styling and handling + future chatbox logic) -->
                 {#if newMessage}
                     <h2 class="text-secondary text-2xl font-bold">
-                        {$t(`reviews.risks-cards.types.${riskType}.title`)}
+                        {review.title}
                     </h2>
                     <span class="text-content text-sm/4">32 chats. Última actividad 12/11/2025</span
                     >
                 {:else}
                     <h2 class="text-secondary text-2xl font-bold">
-                        {$t(`reviews.risks-cards.types.${riskType}.title`)}
+                        {review.title}
                     </h2>
                     <span class="text-content text-sm/4">32 chats. Última actividad 12/11/2025</span
                     >
                 {/if}
             </div>
-            <Tag variant={tagVariant}>{$t(`reviews.risks-cards.tags.${riskTier}`)}</Tag>
+            <Tag variant={tagVariant}>{$t(`reviews.risks-cards.tags.${review.risk}`)}</Tag>
         </div>
         <p class="text-content line-clamp-4 w-full text-base">
-            {$t(`reviews.risks-cards.types.${riskType}.description.${riskTier}`)}
+            {review.summary}
         </p>
     </div>
     <div class="flex justify-between">
