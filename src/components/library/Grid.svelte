ad9927e39cea4a0329b40d439a645e6d46633172
Dayv1dDev <david.moreno@platoniq.net>
2026-02-16 14:42:41 +0100

    fix: unnecessary use of twMerge for default class names

--- src/components/library/Grid.svelte
+++ src/components/library/Grid.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
     import { twMerge, type ClassNameValue } from "tailwind-merge";
 
-    let { class: classes = "gap-6 lg:grid-cols-3", children } = $props<{
+    let { class: classes = "", children } = $props<{
         class?: ClassNameValue;
         children: any;
     }>();
 </script>
 
-<div class={twMerge("grid", classes)}>
+<div class={twMerge("grid gap-6 lg:grid-cols-3", classes)}>
     {@render children()}
 </div>
