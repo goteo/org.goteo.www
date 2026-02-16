cd46b104e02f0083e45067e91d8e097c607cd8cc
Dayv1dDev <david.moreno@platoniq.net>
2026-02-16 12:15:21 +0100

    fix: add twMerge + modify styles to match Figma in Checkbox library component

--- src/components/library/Checkbox.svelte
+++ src/components/library/Checkbox.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
-    let { id, label, checked = $bindable(false) } = $props<{
+    import { twMerge, type ClassNameValue } from "tailwind-merge";
+
+    let {
+        id,
+        label,
+        checked = $bindable(false),
+        class: classes,
+    } = $props<{
         id?: string;
         label?: string;
         checked?: boolean;
+        class?: ClassNameValue;
     }>();
 </script>
 
-<input
-    type="checkbox"
-    {id}
-    bind:checked
-    class="text-primary border-secondary mt-1 size-5 shrink-0 rounded-sm border ring-0"
-/>
-{#if label}
-    <label for={id} class="text-content text-base font-normal">{label}</label>
-{/if}
+<div class="flex items-center justify-center gap-2">
+    <input
+        type="checkbox"
+        {id}
+        bind:checked
+        class={twMerge(
+            "border-secondary text-primary ring-white size-5 shrink-0 rounded-sm border ring-0 accent-secondary outline-0",
+            classes,
+        )}
+    />
+    {#if label}
+        <label for={id} class="text-content text-base font-normal">{label}</label>
+    {/if}
+</div>
