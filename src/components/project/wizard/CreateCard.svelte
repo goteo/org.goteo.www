4579546c7e21bd9809f99a260d53bdd404cd736d
Dayv1dDev <david.moreno@platoniq.net>
2026-02-24 10:31:38 +0100

    fix: rename CreateCard component for rewards and collabs wizard tabs adding title & description props

--- src/components/project/wizard/CreateCard.svelte
+++ src/components/project/wizard/CreateCard.svelte
@@ -1,21 +1,30 @@
 <script lang="ts">
     import { t } from "../../../i18n/store";
-    import type { Project, ProjectReward } from "../../../openapi/client";
+    import type { Project, ProjectCollaboration, ProjectReward } from "../../../openapi/client";
     import PlusIcon from "../../../svgs/PlusIcon.svelte";
     import Button from "../../library/Button.svelte";
+    import WizardCollabModal from "./WizardCollabModal.svelte";
     import WizardRewardModal from "./WizardRewardModal.svelte";
 
     let {
+        title,
+        description,
         onclick,
+        variant,
         open = $bindable(false),
         project,
         reward,
+        collab,
         onSave,
     } = $props<{
+        title: string;
+        description: string;
         onclick: () => void;
+        variant: "reward" | "collab";
         open: boolean;
         project: Project;
-        reward: ProjectReward | null;
+        reward?: ProjectReward | null;
+        collab?: ProjectCollaboration | null;
         onSave: (data: any) => Promise<void>;
     }>();
 </script>
@@ -26,20 +35,26 @@
     <div class="flex flex-col gap-4">
         <h2 class="text-soft-purple self-stretch text-[40px] leading-12 font-bold">
             <!-- {$t("wizard.steps.rewards.createCard.title")} -->
-            ¡No Lances Tu Campaña Sin Ellas! Las recompensas son tu superpoder
+            {title}
         </h2>
         <p class="text-variant1 self-stretch text-base font-normal">
             <!-- {$t("wizard.steps.rewards.createCard.description")} -->
-            Las donaciones aumentan un 30%* en campañas con múltiples y atractivas recompensas. Es simple:
-            más opciones significan más posibilidades de conectar con los intereses de potenciales donantes.
-            ¿Productos exclusivos? ¿Experiencias únicas? ¿Reconocimientos especiales? ¡Tu creatividad
-            es el límite! Cuanto más variado sea tu menú de recompensas, mayor será tu éxito.
+            {description}
         </p>
     </div>
     <Button kind="secondary" class="flex w-full items-center justify-center gap-2" {onclick}>
         <PlusIcon class="p-[2.25px]" />
-        <!-- {$t("wizard.steps.rewards.createCard.btn")} -->
-        Añadir recompensa
+        {#if variant === "reward"}
+            <!-- {$t("wizard.steps.rewards.createCard.btn")} -->
+            Añadir recompensa
+        {:else if variant === "collab"}
+            <!-- {$t("wizard.steps.collabs.createCard.btn")} -->
+            Añadir colaboración
+        {/if}
     </Button>
 </div>
-<WizardRewardModal bind:open {project} {onSave} {reward} />
+{#if variant === "reward"}
+    <WizardRewardModal bind:open {project} {onSave} {reward} />
+{:else if variant === "collab"}
+    <WizardCollabModal bind:open {project} {onSave} {collab} />
+{/if}
