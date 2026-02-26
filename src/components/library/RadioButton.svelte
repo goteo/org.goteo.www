4f50abc37b694ce919c58ae4ad3b7075432fedcf
Dayv1dDev <david.moreno@platoniq.net>
2026-02-26 12:35:06 +0100

    fix: some styles in RadioButton library component

--- src/components/library/RadioButton.svelte
+++ src/components/library/RadioButton.svelte
@@ -19,14 +19,14 @@
             type="radio"
             bind:group
             class={twMerge(
-                "checked:bg-primary checked:border-primary checked:text-primary border-secondary after:bg-secondary relative h-6 w-6 appearance-none rounded-full border bg-white after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:content-[''] checked:after:block focus:shadow-none focus:outline-0",
+                "checked:bg-primary checked:border-primary checked:text-primary border-secondary after:bg-secondary relative h-6 w-6 appearance-none rounded-full border bg-white ring-0 after:absolute after:top-1/2 after:left-1/2 after:hidden after:h-2 after:w-2 after:-translate-x-1/2 after:-translate-y-1/2 after:transform after:rounded-full after:content-[''] checked:ring-0 checked:after:block focus:shadow-none focus:outline-0",
                 classes,
             )}
             {...rest}
         />
     </div>
     {#if label}
-        <span class="text-gray-700">
+        <span class="text-content">
             {label}
         </span>
     {/if}
