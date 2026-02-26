9f3aca5a8de22dca667d6d034bb7e50ef07ddfc5
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-02-26 15:21:34 +0100

    feat: share oauth access token with svelte via LocalStorage component

---
+++ src/components/LocalStorage.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let { set, remove }: { set: any; remove: any } = $props();
+
+    $effect(() => {
+        const toSet = Object.entries(set);
+
+        for (const [key, value] of toSet) {
+            localStorage.setItem(key, JSON.stringify(value));
+        }
+    });
+
+    $effect(() => {
+        const toRemove = Object.entries(remove);
+
+        for (const [key] of toRemove) {
+            localStorage.removeItem(key);
+        }
+    });
+</script>
