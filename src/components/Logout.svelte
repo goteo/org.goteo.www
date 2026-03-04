db43e010d8a7445c09906d33b8ae91fc2ca2eff6
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-03-04 14:24:19 +0100

    feat: start port Header component to svelte

--- src/components/Logout.svelte
+++ src/components/Logout.svelte
@@ -1,16 +1,6 @@
----
-const { t } = Astro.locals;
----
-
-<button
-    id="btn-logout"
-    class="text-secondary hover:bg-grey block w-full cursor-pointer overflow-hidden px-4 py-2 font-bold overflow-ellipsis"
->
-    {t("logout.label")}
-</button>
-
 <script>
     import { actions } from "astro:actions";
+    import { t } from "../i18n/store";
 
     const button = document.querySelector("#btn-logout");
     button?.addEventListener("click", async () => {
@@ -21,3 +11,10 @@ const { t } = Astro.locals;
         window.location.href = `/`;
     });
 </script>
+
+<button
+    id="btn-logout"
+    class="text-secondary hover:bg-grey block w-full cursor-pointer overflow-hidden px-4 py-2 font-bold overflow-ellipsis"
+>
+    {$t("logout.label")}
+</button>
