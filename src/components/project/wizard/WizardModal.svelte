d7c5a46ada8c840612e50d031de1166c2cbe966c
Dayv1dDev <david.moreno@platoniq.net>
2026-03-13 13:32:42 +0100

    fix: add types to payload for budget variant + fix bugs for CreateCard modal + better selectedStep handling in WizardModal component

--- src/components/project/wizard/WizardModal.svelte
+++ src/components/project/wizard/WizardModal.svelte
@@ -21,6 +21,8 @@
         money?: { amount: number; currency: string };
         isFinite?: boolean;
         unitsTotal?: number | null;
+        type?: "infrastructure" | "material" | "task";
+        deadline?: "minimum" | "optimum";
     };
 
     let {
@@ -51,8 +53,10 @@
     let unlimited = $state(reward?.isFinite ?? false);
     let files = $state<File[]>([]);
 
-    let selectedBudgetType = $state(budgetItem?.type ?? "");
-    let selectedBudgetDeadline = $state(budgetItem?.deadline ?? "");
+    let selectedBudgetDeadline: "minimum" | "optimum" | undefined = $state(budgetItem?.deadline);
+    let selectedBudgetType: "infrastructure" | "material" | "task" | undefined = $state(
+        budgetItem?.type,
+    );
 
     let selectedStep: string = $state("");
     const INPUTS_CLASSES: ClassNameValue =
@@ -76,6 +80,12 @@
                 isFinite: unlimited ? false : true,
                 unitsTotal: unlimited ? null : rewardCount,
             };
+        } else if (budgetItem) {
+            payload = {
+                ...payload,
+                deadline: selectedBudgetDeadline,
+                type: selectedBudgetType,
+            };
         }
 
         await onSave?.(payload);
@@ -92,16 +102,20 @@
     function handleSelectedDeadline() {}
 
     $effect(() => {
-        if (!collab && !budgetItem && reward) selectedStep = "rewards";
-        else if (!reward && !budgetItem && collab) selectedStep = "collabs";
-        else if (!reward && !collab && budgetItem) selectedStep = "budget";
+        if (!collab && !budgetItem && reward || reward === null) selectedStep = "rewards";
+        else if (!reward && !budgetItem && collab || collab === null) selectedStep = "collabs";
+        else if (!reward && !collab && budgetItem || budgetItem === null) {
+            selectedStep = "budget";
+            selectedBudgetDeadline = budgetItem?.deadline;
+            selectedBudgetType = budgetItem?.type;
+        }
     });
 </script>
 
 <Modal
     bind:open
     closeBtnClass="top-7 end-7 cursor-pointer bg-transparent text-secondary hover:bg-transparent hover:text-secondary hover:scale-110 transition-transform duration-200 transform focus:ring-0 shadow-none dark:text-secondary dark:hover:text-secondary dark:hover:bg-transparent"
-    class="fixed top-1/2 left-1/2 flex w-full max-w-225 -translate-x-1/2 -translate-y-1/2 flex-col gap-8 rounded-3xl bg-white p-6 shadow-lg backdrop:bg-[#878282B2] backdrop:backdrop-blur-[5px]"
+    class="fixed top-1/2 left-1/2 mx-2 flex w-full max-w-225 -translate-x-1/2 -translate-y-1/2 flex-col gap-8 rounded-3xl bg-white p-6 shadow-lg backdrop:bg-[#878282B2] backdrop:backdrop-blur-[5px] sm:mx-4 lg:mx-0"
     headerClass="md:p-0 p-0 flex-col gap-4 justify-start items-start"
     bodyClass="md:p-0 p-0"
     footerClass="md:p-0 p-0 flex items-center justify-end gap-4"
@@ -115,46 +129,44 @@
         </p>
     {/snippet}
     <div class="flex flex-col gap-4">
-        {#if reward}
+        {#if selectedStep === "rewards"}
             <input
                 type="text"
                 placeholder={$t("wizard.rewards.modal.placeholders.title")}
                 class={INPUTS_CLASSES}
-                value={reward.title ?? ""}
+                value={reward?.title ?? ""}
             />
             <textarea
                 placeholder={$t("wizard.rewards.modal.placeholders.description")}
-                class={`h-32 resize-none ${INPUTS_CLASSES}`}>{reward.description ?? ""}</textarea
+                class={`h-32 resize-none ${INPUTS_CLASSES}`}>{reward?.description ?? ""}</textarea
             >
             <input
+                bind:value={moneyAmount}
                 type="text"
                 placeholder={$t("wizard.rewards.modal.placeholders.moneyAmount")}
                 class={INPUTS_CLASSES}
-                value={reward.money.amount && reward.money.currency
-                    ? formatCurrency(reward.money.amount, reward.money.currency)
-                    : ""}
             />
             <div class="flex flex-col gap-6">
                 <FileUpload bind:files />
                 <RewardItemsSelector bind:value={rewardCount} bind:unlimited />
             </div>
-        {:else if collab}
+        {:else if selectedStep === "collabs"}
             <input
                 type="text"
                 placeholder={$t("wizard.collabs.modal.placeholders.title")}
                 class={INPUTS_CLASSES}
-                value={collab.title ?? ""}
+                value={collab?.title ?? ""}
             />
             <textarea
                 placeholder={$t("wizard.collabs.modal.placeholders.description")}
-                class={`h-32 resize-none ${INPUTS_CLASSES}`}>{collab.description ?? ""}</textarea
+                class={`h-32 resize-none ${INPUTS_CLASSES}`}>{collab?.description ?? ""}</textarea
             >
-        {:else if budgetItem}
+        {:else if selectedStep === "budget"}
             <input
                 type="text"
                 placeholder={$t("wizard.budget.modal.placeholders.title")}
                 class={INPUTS_CLASSES}
-                value={budgetItem.title ?? ""}
+                value={budgetItem?.title ?? ""}
             />
             <select
                 bind:value={selectedBudgetType}
@@ -163,12 +175,10 @@
                 title={$t("wizard.budget.modal.selectors.types.title")}
                 class={`${INPUTS_CLASSES} `}
             >
-                <option value="" selected={!budgetItem.type ? true : false}
+                <option value="" selected={!budgetItem?.type ? true : false}
                     >{$t("wizard.budget.modal.selectors.types.placeholder")}</option
                 >
-                <option
-                    value="infrastructure"
-                    selected={budgetItem.type && budgetItem.type === "infrastructure"}
+                <option value="infrastructure"
                     >{$t("wizard.budget.modal.selectors.types.infrastructure")}</option
                 >
                 <option value="material"
@@ -176,13 +186,13 @@
                 >
                 <option value="task">{$t("wizard.budget.modal.selectors.types.task")}</option>
             </select>
-            <div class="flex">
+            <div class="flex gap-4">
                 <input
                     type="text"
                     placeholder={$t("wizard.budget.modal.placeholders.amount")}
-                    class={INPUTS_CLASSES}
-                    value={budgetItem.money.amount && budgetItem.money.currency
-                        ? formatCurrency(budgetItem.money.amount, budgetItem.money.currency)
+                    class={`${INPUTS_CLASSES} w-[50%]`}
+                    value={budgetItem?.money.amount && budgetItem?.money.currency
+                        ? formatCurrency(budgetItem?.money.amount, budgetItem?.money.currency)
                         : ""}
                 />
                 <select
@@ -190,21 +200,21 @@
                     onchange={handleSelectedDeadline}
                     aria-label={$t("wizard.budget.modal.selectors.deadline.title")}
                     title={$t("wizard.budget.modal.selectors.deadline.title")}
-                    class={`${INPUTS_CLASSES} `}
+                    class={`${INPUTS_CLASSES} w-[50%]`}
                     required
                 >
-                    <option value="" selected={!budgetItem.deadline ? true : false}
+                    <option value="" selected={!budgetItem?.deadline ? true : false}
                         >{$t("wizard.budget.modal.selectors.deadline.placeholder")}</option
                     >
                     <option
-                        selected={selectedBudgetDeadline && selectedBudgetDeadline === "minimum"
+                        selected={budgetItem?.deadline && budgetItem?.deadline === "minimum"
                             ? true
                             : false}
                         value="minimum"
                         >{$t("wizard.budget.modal.selectors.deadline.minimum")}</option
                     >
                     <option
-                        selected={selectedBudgetDeadline && selectedBudgetDeadline === "optimum"
+                        selected={budgetItem?.deadline && budgetItem?.deadline === "optimum"
                             ? true
                             : false}
                         value="optimum"
@@ -214,14 +224,14 @@
             </div>
             <textarea
                 placeholder={$t("wizard.budget.modal.placeholders.description")}
-                class={`h-32 resize-none ${INPUTS_CLASSES}`}
-                >{budgetItem.description ?? ""}</textarea
+                class={`${INPUTS_CLASSES} h-60 resize-none `}
+                >{budgetItem?.description ?? ""}</textarea
             >
         {/if}
     </div>
 
     {#snippet footer()}
-        {#if reward !== null || collab !== null || budgetItem !== null}
+        {#if reward !== null && collab !== null && budgetItem !== null}
             <Button kind="secondary" onclick={() => handleDeleteClick()} class="w-fit">
                 {$t(`wizard.${selectedStep}.modal.btns.delete`)}
             </Button>
