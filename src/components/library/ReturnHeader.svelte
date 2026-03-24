e9dbbc834392c64760a9f2c2254ca2c1a28c7075
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-03-24 10:58:52 +0100

    feat: Back button implemented

---
+++ src/components/library/ReturnHeader.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import Return from "./Return.svelte";
+</script>
+
+<div class="w-full px-2 md:px-6">
+    <div class="mx-auto max-w-[1440px] px-2 pl-5">
+        <Return />
+    </div>
+</div>
