dabb3da972794b638795c6dc9cb4ad48296831ad
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-04-03 18:07:52 +0200

    Merge branch 'develop' into feat/project-submission

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,7 @@
-import { login } from "./login";
-import { logout } from "./logout";
 import { payment } from "./payment";
 import { register } from "./register";
 
 export const server = {
-    login,
-    logout,
     register,
     payment,
 };
