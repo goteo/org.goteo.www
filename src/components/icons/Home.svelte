9cee44038010ee171d6ae5f4591d978e2b3e49f4
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-07 13:34:25 +0200

    feat: implement component icon Home

---
+++ src/components/icons/Home.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    import { twMerge } from "tailwind-merge";
+
+    interface Props {
+        class?: string;
+        width?: string | number;
+        height?: string | number;
+    }
+
+    let { class: classes = "", width = "24", height = "24" }: Props = $props();
+</script>
+
+<svg
+    {width}
+    {height}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class={twMerge("text-secondary", classes)}
+>
+    <foreignObject x="-4" y="-4" width="32" height="32"
+        ><div
+            xmlns="http://www.w3.org/1999/xhtml"
+            style="backdrop-filter:blur(2px);clip-path:url(#bgblur_1_501_2117_clip_path);height:100%;width:100%"
+        ></div></foreignObject
+    ><g data-figma-bg-blur-radius="4">
+        <g clip-path="url(#clip0_501_2117)">
+            <rect width="24" height="24" rx="12" fill="white" fill-opacity="0.01" />
+            <circle
+                opacity="0.08"
+                cx="12"
+                cy="12"
+                r="11.75"
+                fill="var(--color-secondary)"
+                stroke="var(--color-variant1)"
+                stroke-width="0.5"
+            />
+            <circle opacity="0.8" cx="12" cy="12" r="10" fill="var(--color-secondary)" />
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M12 6L18 13H17.5C16.9477 13 16.5 13.4477 16.5 14V17C16.5 17.5523 16.0523 18 15.5 18H13V14H11V18H8.5C7.94772 18 7.5 17.5523 7.5 17V14C7.5 13.4477 7.05228 13 6.5 13H6L12 6Z"
+                fill="var(--color-primary)"
+            />
+        </g>
+    </g>
+    <defs>
+        <clipPath id="bgblur_1_501_2117_clip_path" transform="translate(4 4)"
+            ><rect width="24" height="24" rx="12" />
+        </clipPath><clipPath id="clip0_501_2117">
+            <rect width="24" height="24" rx="12" fill="white" />
+        </clipPath>
+    </defs>
+</svg>
