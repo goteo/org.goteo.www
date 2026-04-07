8befaa5a4ed958dc7c8d032a7dbc35ed206cb20a
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-07 12:48:59 +0200

    feat: implement component icon  APIKey

---
+++ src/components/icons/APIKey.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { twMerge } from "tailwind-merge";
+
+    interface Props {
+        class?: string;
+        width?: string | number;
+        height?: string | number;
+    }
+
+    let { class: classes = "", width = "40", height = "40" }: Props = $props();
+</script>
+
+<svg
+    {width}
+    {height}
+    viewBox="0 0 40 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class={twMerge("text-secondary", classes)}
+>
+    <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M24.0001 4L21.3334 17.3336H29.3335L15.9999 35.9981L18.6666 22.6658H10.6665L24.0001 4Z"
+        stroke="currentColor"
+        stroke-width="2.66671"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
