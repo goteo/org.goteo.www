901ef4262b2e9697cb2787652db709f15e30ffa7
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-07 13:49:03 +0200

    feat: implement component icon ShortBy

---
+++ src/components/icons/ShortBy.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { twMerge } from "tailwind-merge";
+
+    interface Props {
+        class?: string;
+        width?: string | number;
+        height?: string | number;
+    }
+
+    let { class: classes = "", width = "8", height = "14" }: Props = $props();
+</script>
+
+<svg
+    {width}
+    {height}
+    viewBox="0 0 8 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class={twMerge("text-secondary", classes)}
+>
+    <path d="M4 0L8 6H0L4 0Z" fill="currentColor" />
+    <path d="M4 14L5.24537e-07 8L8 8L4 14Z" fill="currentColor" />
+</svg>
