ecbbc6f887982a3a363106945aa48e22f4262258
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-07 11:06:29 +0200

    feat: implement component icon Idiomas

---
+++ src/components/icons/Idiomas.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { twMerge } from "tailwind-merge";
+
+    interface Props {
+        class?: string;
+        width?: string | number;
+        height?: string | number;
+    }
+
+    let { class: classes = "", width = "24", height = "24" }: Props = $props();
+</script>
+
+<svg
+    {width}
+    {height}
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class={twMerge("text-secondary", classes)}
+>
+    <path
+        d="M13 14H12M17 14H16M21 14H20M5 7V27L10 23H25C26.1046 23 27 22.1046 27 21V7C27 5.89543 26.1046 5 25 5H7C5.89543 5 5 5.89543 5 7Z"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+    />
+</svg>
