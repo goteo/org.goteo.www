9c7bb594d707dfa6931f1b358a7b082b0ef4270d
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-10 14:12:00 +0200

    fix: consolidate App.Locals types into root env.d.ts

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="astro/client" />
-
-declare namespace App {
-    interface Locals {
-        lang: import("./i18n/locales/index").Locale;
-        t: ReturnType<typeof import("./i18n/utils").useTranslations>;
-        session: import("./auth/types").Session | undefined;
-    }
-}
