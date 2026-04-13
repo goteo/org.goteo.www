e60479490be4169353817c10464866d8a4bb4da5
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-13 14:32:53 +0200

    test: implement component ChatTextarea

---
+++ src/components/library/ChatTextarea.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import SendIcon from "../icons/SendIcon.svelte";
+</script>
+
+<div class="flex items-center gap-6">
+    <div class="bg-grey flex flex-1 items-center gap-2 rounded-lg p-4 shadow-sm">
+        <textarea
+            class="placeholder:text-content flex-1 border-none bg-transparent outline-none focus:ring-0 focus:outline-none"
+            placeholder="Escriba aquí"
+            rows="1"
+        ></textarea>
+    </div>
+    <button
+        class="border-grey flex aspect-square h-10 w-10 shrink-0 items-center justify-center rounded-full border bg-white p-2 shadow-md"
+    >
+        <SendIcon />
+    </button>
+</div>
