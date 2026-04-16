052d77a43f9d547a0647bbb52372f36776adcd87
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-16 11:10:05 +0200

    Fix: component Home

--- src/components/icons/Home.svelte
+++ src/components/icons/Home.svelte
@@ -25,7 +25,7 @@
         ></div></foreignObject
     ><g data-figma-bg-blur-radius="4">
         <g clip-path="url(#clip0_501_2117)">
-            <rect width="24" height="24" rx="12" fill="white" fill-opacity="0.01" />
+            <rect width="24" height="24" rx="12" fill="var(--color-white)" fill-opacity="0.01" />
             <circle
                 opacity="0.08"
                 cx="12"
@@ -40,7 +40,7 @@
                 fill-rule="evenodd"
                 clip-rule="evenodd"
                 d="M12 6L18 13H17.5C16.9477 13 16.5 13.4477 16.5 14V17C16.5 17.5523 16.0523 18 15.5 18H13V14H11V18H8.5C7.94772 18 7.5 17.5523 7.5 17V14C7.5 13.4477 7.05228 13 6.5 13H6L12 6Z"
-                fill="var(--color-primary)"
+                fill="currentColor"
             />
         </g>
     </g>
