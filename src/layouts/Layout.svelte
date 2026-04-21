4b4806a0500b974893b64871a1d79d5619140453
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-21 13:37:27 +0200

    fix: remove meta charset from layout

--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -8,7 +8,6 @@
 </script>
 
 <svelte:head>
-    <meta charset="UTF-8" />
     <meta http-equiv="x-ua-compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
     <title>{title}</title>
