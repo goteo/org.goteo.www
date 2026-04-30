5d9f0b263f85fb5913bf1add97cfa06da6e57f54
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-04-30 14:06:00 +0200

    chore: prettier

--- src/components/CartButton.svelte
+++ src/components/CartButton.svelte
@@ -3,29 +3,16 @@
     import { t } from "../i18n/store";
     import { itemCount } from "../stores/cart";
 
-    // Browser check for SSR compatibility
     const browser = typeof window !== "undefined";
 
-    // Use reactive store directly in Svelte 5 - safer for SSR
     let count = 0;
 
-    // Reactive statement to update count when store changes
     $: if (browser) {
         count = $itemCount;
     }
 
     function handleDirect() {
         if (!browser) return;
-
-        // const pathParts = window.location.pathname.split("/").filter(Boolean);
-        // console.log(pathParts);
-        // const languages = Object.keys(languagesList) as Locale[];
-        // console.log(languages);
-        // const currentLang: Locale = languages.includes(pathParts[0] as Locale)
-        //     ? (pathParts[0] as Locale)
-        //     : "es";
-        // console.log(currentLang);
-
         window.location.href = `/checkout`;
     }
 </script>
