a3010efe9ea4850560aa8838da0464a127fb8fa1
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-05-06 11:54:29 +0200

    fix: correctly apply tailwind

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,21 +1,16 @@
-import type { StorybookConfig } from '@storybook/svelte-vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite";
+
+import type { StorybookConfig } from "@storybook/svelte-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.stories.@(js|ts|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-svelte-csf"
-  ],
-  "framework": "@storybook/svelte-vite",
-  async viteFinal(config) {
-    config.plugins = [
-      svelte(),
-      ...(config.plugins || []),
-    ];
+    stories: ["../src/**/*.stories.@(js|ts|svelte)"],
+    addons: ["@storybook/addon-svelte-csf"],
+    framework: "@storybook/svelte-vite",
+    async viteFinal(config) {
+        config.plugins = [tailwindcss(), svelte(), ...(config.plugins || [])];
 
-    return config;
-  }
+        return config;
+    },
 };
 export default config;
