4fdcf3eebfb2bbff0fab95d0b9409e9ecca9b802
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-05-06 13:57:22 +0200

    fix: add addon on main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,12 @@ import type { StorybookConfig } from "@storybook/svelte-vite";
 const config: StorybookConfig = {
     stories: ["../src/**/*.stories.@(js|ts|svelte)"],
     staticDirs: ["../public"],
-    addons: ["@storybook/addon-svelte-csf", "@storybook/addon-docs"],
+    addons: [
+        "@storybook/addon-svelte-csf",
+        "@storybook/addon-docs",
+        "@storybook/addon-vitest",
+        "@storybook/addon-a11y"
+    ],
     framework: "@storybook/svelte-vite",
     async viteFinal(config) {
         config.plugins = [tailwindcss(), svelte(), ...(config.plugins || [])];
