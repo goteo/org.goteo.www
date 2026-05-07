523df92d44b0e24a730444dc800b492facc488ca
Pep Villalonga Amorós <pepvillalonga312345@gmail.com>
2026-05-07 14:05:48 +0200

    feat: implement story ChatTextarea

---
+++ src/components/library/admin/ChatTextarea.stories.svelte
@@ -0,0 +1,13 @@
+<script module>
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+
+    import ChatTextarea from "./ChatTextarea.svelte";
+
+    const { Story } = defineMeta({
+        component: ChatTextarea,
+        title: "Library/Admin/ChatTextarea",
+        tags: ["autodocs"],
+    });
+</script>
+
+<Story name="Default" />
