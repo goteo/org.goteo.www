bb403455a68ad0043bc7fcd7d741deaac256bfe0
Angel Di Pinto <angel.dipinto@platoniq.net>
2026-05-19 10:45:23 +0200

    fix: Steps.svelte

--- src/components/Checkout/Steps.svelte
+++ src/components/Checkout/Steps.svelte
@@ -11,19 +11,21 @@
         $t("checkout.steps.payment.title"),
         $t("checkout.steps.confirmation.title"),
     ];
+
+    $: progressPercentage = ((step - 1) / (stepsLabels.length - 1)) * 100;
 </script>
 
 <div class="w-full">
-    <div class="relative">
+    <div class="relative px-2">
         <div class="bg-variant1 h-4 w-full rounded-full"></div>
 
         <div
-            class="bg-primary absolute top-0 left-0 h-4 rounded-full transition-all duration-500"
-            style="width: {Math.min(((2 * step - 1) / 6) * 100, 100)}%;"
+            class="bg-primary absolute top-0 left-2 h-4 rounded-full transition-all duration-500"
+            style="width: calc({progressPercentage}%);"
         ></div>
 
         <div
-            class="absolute top-1/2 flex w-full -translate-y-1/2 items-center justify-between px-2"
+            class="absolute top-1/2 left-0 flex w-full -translate-y-1/2 items-center justify-between px-2"
         >
             {#each [1, 2, 3, 4] as i}
                 <div
@@ -35,21 +37,22 @@
         </div>
     </div>
 
-    <div class="mt-4 flex w-full items-center justify-between text-sm font-bold text-gray-700">
+    <div class="mt-3 flex w-full justify-between">
         {#each stepsLabels as label, index}
-            <div
-                class={`flex max-w-20 items-center gap-1 text-center ${
-                    step === index + 1 && hasError
-                        ? "text-black"
-                        : step >= index + 1
-                          ? "text-black"
-                          : "text-gray-400"
-                }`}
-            >
-                {#if step === index + 1 && hasError}
-                    <WarningIcon class="h-4 w-4" />
-                {/if}
-                <span class="text-xs leading-tight">{label}</span>
+            {@const active = step === index + 1}
+
+            <div class="flex flex-col items-center px-1 text-center">
+                <div class="flex min-h-6 items-center justify-center gap-1">
+                    {#if active && hasError}
+                        <WarningIcon class="h-4 w-4 shrink-0" />
+                    {/if}
+                    <span
+                        class="font-karla text-[14px] leading-6 font-bold
+                        {step >= index + 1 ? 'text-secondary' : 'text-gray-400/60'}"
+                    >
+                        {label}
+                    </span>
+                </div>
             </div>
         {/each}
     </div>
