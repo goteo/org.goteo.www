d432ce8d00264906b9ebdf1756e48279eae57e0e
Angel Di Pinto <angel.dipinto@platoniq.net>
2026-05-19 10:39:22 +0200

    new Checkbox.svelte

---
+++ src/components/library/Checkbox.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { twMerge, type ClassNameValue } from "tailwind-merge";
+
+    import type { Snippet } from "svelte";
+
+    let {
+        id,
+        label,
+        children,
+        checked = $bindable(false),
+        class: classes,
+    } = $props<{
+        id?: string;
+        label?: string;
+        children?: Snippet;
+        checked?: boolean;
+        class?: ClassNameValue;
+    }>();
+</script>
+
+<div class="flex items-start justify-center gap-2"> <input
+        type="checkbox"
+        {id}
+        bind:checked
+        class={twMerge(
+            "border-secondary text-primary ring-white size-5 shrink-0 rounded-sm border ring-0 accent-secondary outline-0 mt-0.5",
+            classes,
+        )}
+    />
+    
+    <label for={id} class="text-content text-base font-normal">
+        {#if children}
+            {@render children()}
+        {:else if label}
+            {label}
+        {/if}
+    </label>
+</div>
