47e760070666bd0d22c1abee853963299dafcf2c
Pep Villalonga <pep.villalonga@patoniq.net>
2026-06-17 11:39:10 +0200

    chore: format

--- src/components/home/CampaignsTabsNav.svelte
+++ src/components/home/CampaignsTabsNav.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import Clock from "../../components/icons/Clock.svelte";
     import { t } from "../../i18n/store";
-    import Flames from "../icons/status/Flames.svelte";
     import Flash from "../icons/media/Flash.svelte";
+    import Flames from "../icons/status/Flames.svelte";
     import Tabs from "../library/layout/Tabs.svelte";
 
     interface Props {
