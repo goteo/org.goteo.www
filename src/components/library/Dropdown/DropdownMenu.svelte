e1a3035c3477239adbfecc3d91c6662de578fb53
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-06-18 17:05:56 +0200

    fix: style of the search input in the dropdown menu did not allow for nice alignment of the component

--- src/components/library/Dropdown/DropdownMenu.svelte
+++ src/components/library/Dropdown/DropdownMenu.svelte
@@ -45,18 +45,16 @@
     class="flex w-full flex-col rounded-lg bg-transparent shadow-[0_35px_10px_0_rgba(0,0,0,0),0_22px_9px_0_rgba(0,0,0,0.01),0_13px_8px_0_rgba(0,0,0,0.05),0_6px_6px_0_rgba(0,0,0,0.09),0_1px_3px_0_rgba(0,0,0,0.1)]"
 >
     {#if hasSearch}
-        <div class="border-grey rounded-t-lg border bg-white p-4">
-            <div
-                class="border-secondary group relative flex items-center justify-between rounded-3xl border bg-white p-4"
-            >
-                <input
-                    class="max-h-6 w-full max-w-72 border-0 bg-white p-0 text-base/6 font-normal text-black ring-0 placeholder:opacity-48"
-                    type="text"
-                    placeholder={searchPlaceholder}
-                    oninput={(e) => onSearch?.(e.currentTarget.value)}
-                />
-                <SearchIcon class="absolute right-4" width="32" height="32" />
-            </div>
+        <div
+            class="group relative flex items-center justify-between rounded-3xl bg-white p-4"
+        >
+            <input
+                class="max-h-6 w-full max-w-72 border-0 bg-white p-0 text-base/6 font-normal text-black ring-0 placeholder:opacity-48"
+                type="text"
+                placeholder={searchPlaceholder}
+                oninput={(e) => onSearch?.(e.currentTarget.value)}
+            />
+            <SearchIcon class="absolute right-4" width="32" height="32" />
         </div>
     {/if}
     {#each renderedItems as item}
