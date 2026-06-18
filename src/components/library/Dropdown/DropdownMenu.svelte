0601deeddd4abaacb5a308c02e0196ab085a27af
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-06-18 17:32:14 +0200

    feat: allow to open/close the dropdown from search input

--- src/components/library/Dropdown/DropdownMenu.svelte
+++ src/components/library/Dropdown/DropdownMenu.svelte
@@ -4,6 +4,7 @@
     import SearchIcon from "../../../svgs/SearchIcon.svelte";
 
     import type { DropdownItemType, DropdownVariant } from "./dropdown.types";
+    import { clickOutside } from "flowbite-svelte";
 
     interface Props {
         variant: DropdownVariant;
@@ -16,6 +17,7 @@
          * @param value Value of the search input
          */
         onSearch?: (value: string) => void;
+        isOpen?: boolean;
     }
 
     let {
@@ -25,6 +27,7 @@
         hasSearch = false,
         searchPlaceholder = $t("domain.search.bar.placeholder"),
         onSearch = undefined,
+        isOpen = true,
     }: Props = $props();
 
     const renderedItems = $derived(
@@ -45,28 +48,30 @@
     class="flex w-full flex-col rounded-lg bg-transparent shadow-[0_35px_10px_0_rgba(0,0,0,0),0_22px_9px_0_rgba(0,0,0,0.01),0_13px_8px_0_rgba(0,0,0,0.05),0_6px_6px_0_rgba(0,0,0,0.09),0_1px_3px_0_rgba(0,0,0,0.1)]"
 >
     {#if hasSearch}
-        <div
-            class="group relative flex items-center justify-between rounded-3xl bg-white p-4"
-        >
+        <div class="group relative flex items-center justify-between rounded-3xl bg-white p-4">
             <input
                 class="max-h-6 w-full max-w-72 border-0 bg-white p-0 text-base/6 font-normal text-black ring-0 placeholder:opacity-48"
                 type="text"
                 placeholder={searchPlaceholder}
                 oninput={(e) => onSearch?.(e.currentTarget.value)}
+                onclick={() => isOpen = !isOpen}
+                use:clickOutside={() => isOpen = false}
             />
             <SearchIcon class="absolute right-4" width="32" height="32" />
         </div>
     {/if}
-    {#each renderedItems as item}
-        <DropdownItem
-            {...item}
-            {variant}
-            bind:selectedIds
-            class={item.position === "start"
-                ? "rounded-t-lg"
-                : item.position === "end"
-                  ? "rounded-b-lg"
-                  : ""}
-        />
-    {/each}
+    {#if isOpen}
+        {#each renderedItems as item}
+            <DropdownItem
+                {...item}
+                {variant}
+                bind:selectedIds
+                class={item.position === "start"
+                    ? "rounded-t-lg"
+                    : item.position === "end"
+                      ? "rounded-b-lg"
+                      : ""}
+            />
+        {/each}
+    {/if}
 </div>
