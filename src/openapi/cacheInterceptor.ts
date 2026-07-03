8a71f9ee25d4d4cb0116c5e38465e9fbaaf2ff00
Dayv1dDev <david.moreno@platoniq.net>
2026-07-03 11:58:21 +0200

    fix: format by prettier

--- src/openapi/cacheInterceptor.ts
+++ src/openapi/cacheInterceptor.ts
@@ -5,26 +5,26 @@ let _interceptor: ReturnType<typeof createBrowserCacheInterceptor> | null = null
 let _interceptorId: number | null = null;
 
 export function getCacheInterceptor() {
-  return _interceptor;
+    return _interceptor;
 }
 
 export function getCacheInterceptorId() {
-  return _interceptorId;
+    return _interceptorId;
 }
 
 export function initCacheInterceptor() {
-  _interceptor = createBrowserCacheInterceptor();
-  _interceptorId = client.interceptors.request.use(_interceptor);
-  return _interceptorId;
+    _interceptor = createBrowserCacheInterceptor();
+    _interceptorId = client.interceptors.request.use(_interceptor);
+    return _interceptorId;
 }
 
 export async function withoutCache<T>(fn: () => Promise<T>): Promise<T> {
-  if (!_interceptor) return fn();
-  
-  client.interceptors.request.eject(_interceptor);
-  try {
-    return await fn();
-  } finally {
-    client.interceptors.request.use(_interceptor);
-  }
+    if (!_interceptor) return fn();
+
+    client.interceptors.request.eject(_interceptor);
+    try {
+        return await fn();
+    } finally {
+        client.interceptors.request.use(_interceptor);
+    }
 }
