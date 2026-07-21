757481485f07da5c4a11695f4c167c9db0b2f5b1
Pep Villalonga <pep.villalonga@platoniq.net>
2026-07-21 12:04:23 +0200

    chore: comply with eslint 10 recommended rules

--- src/components/admin/Table.svelte
+++ src/components/admin/Table.svelte
@@ -113,8 +113,6 @@
     }
 
     function addChargesMetadata(charges: ExtendedCharge[]) {
-        let hasConcept = false;
-
         for (const charge of charges) {
             const targetAcc = accountingsMap.get(charge.target ?? "") as Accounting | undefined;
             const originAcc = accountingsMap.get(charge.checkoutOrigin ?? "") as
@@ -124,9 +122,7 @@
             const targetName = getDisplayNameFromAccounting(targetAcc, ownersMap);
             const originName = getDisplayNameFromAccounting(originAcc, ownersMap);
 
-            hasConcept = false;
-
-            if (targetName === originName) hasConcept = true;
+            const hasConcept = targetName === originName;
 
             charge.targetDisplayName = typeof targetName === "undefined" ? "—" : targetName;
             charge.originDisplayName = typeof originName === "undefined" ? "—" : originName;
