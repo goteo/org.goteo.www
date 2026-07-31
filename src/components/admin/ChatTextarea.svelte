bd0fcc7840f0c2357b2944ff6e9a8a8b26c973ef
Angel Di Pinto <angel.dipinto@platoniq.net>
2026-07-31 11:26:27 +0200

    fix: pnpm format

--- src/components/admin/ChatTextarea.svelte
+++ src/components/admin/ChatTextarea.svelte
@@ -12,7 +12,8 @@
 <div class={twMerge("flex w-full max-w-340 items-center gap-6 self-center", classes)}>
     <textarea
         class="text-content bg-grey h-20 w-full resize-none rounded-lg border-0 p-4 text-base shadow-sm ring-0"
-        placeholder={$t("pages.review.chat.placeholder")}></textarea>
+        placeholder={$t("pages.review.chat.placeholder")}
+    ></textarea>
     <button
         class="border-grey flex aspect-square size-10 shrink-0 cursor-pointer items-center justify-center rounded-[80px] border bg-white p-2 shadow-[0_2px_4px_0_rgba(0,0,0,0.16)]"
         ><Send /></button
