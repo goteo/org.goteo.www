45ba9aeab2d45aa627b6e9c348e49aacea4e5b53
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-08-14 19:10:01 +0200

    fix: bad translation keys in CommentModal

--- src/components/checkout/CommentModal.svelte
+++ src/components/checkout/CommentModal.svelte
@@ -55,7 +55,7 @@
 
 <Modal
     bind:open
-    title={$t("payment.page-approved.form-review.comment-modal.title")}
+    title={$t("pages.checkout.verify.approved.formReview.commentModal.title")}
     closeBtnClass="top-7 end-7 bg-transparent text-secondary hover:bg-transparent hover:text-secondary hover:scale-110 transition-transform duration-200 transform focus:ring-0 shadow-none dark:text-secondary dark:hover:text-secondary dark:hover:bg-transparent"
     class="fixed top-1/2 left-1/2 w-full max-w-225 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-white p-6 shadow-lg backdrop:bg-[#878282B2] backdrop:backdrop-blur-[5px]"
     headerClass="self-start p-0 mb-8 text-2xl font-bold text-content text-ellipsis"
@@ -64,15 +64,18 @@
     <div class="mb-0 h-fit p-0">
         <label
             class="relative top-3 left-4 bg-white px-1 text-sm/6 font-normal text-black"
-            for="comment">{$t("payment.page-approved.form-review.comment-modal.label")}</label
+            for="comment"
         >
+            {$t("pages.checkout.verify.approved.formReview.commentModal.label")}
+        </label>
         <textarea
             class="border-secondary text-content h-30 w-full resize-none rounded-lg border bg-white p-4 text-base/6 font-normal"
             id="comment"
             bind:value={message}
-            placeholder={$t("payment.page-approved.form-review.comment-modal.label")}></textarea>
+            placeholder={$t("pages.checkout.verify.approved.formReview.commentModal.label")}
+        ></textarea>
     </div>
-    <Button onclick={handleSubmit} type={"submit"} class="w-full self-center md:w-fit md:self-end"
-        >{$t("payment.page-approved.form-review.comment-modal.btn")}</Button
-    >
+    <Button onclick={handleSubmit} type={"submit"} class="w-full self-center md:w-fit md:self-end">
+        {$t("common.save")}
+    </Button>
 </Modal>
