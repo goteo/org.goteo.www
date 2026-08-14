2002764854e147cf68655b5106cf668eba903a00
Daniel Subiabre <daniel.subiabre@platoniq.net>
2026-08-14 19:19:10 +0200

    fix: bad styling on CommendModal

--- src/components/checkout/CommentModal.svelte
+++ src/components/checkout/CommentModal.svelte
@@ -3,6 +3,7 @@
     import { t } from "../../i18n/store";
     import Button from "../library/buttons/Button.svelte";
     import { apiProjectSupportsIdPatch, type ProjectSupport } from "../../openapi/client";
+    import TextArea from "../library/inputs/TextArea.svelte";
 
     interface Props {
         open: boolean;
@@ -58,23 +59,13 @@
     title={$t("pages.checkout.verify.approved.formReview.commentModal.title")}
     closeBtnClass="top-7 end-7 bg-transparent text-secondary hover:bg-transparent hover:text-secondary hover:scale-110 transition-transform duration-200 transform focus:ring-0 shadow-none dark:text-secondary dark:hover:text-secondary dark:hover:bg-transparent"
     class="fixed top-1/2 left-1/2 w-full max-w-225 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-white p-6 shadow-lg backdrop:bg-[#878282B2] backdrop:backdrop-blur-[5px]"
-    headerClass="self-start p-0 mb-8 text-2xl font-bold text-content text-ellipsis"
-    bodyClass="p-0 flex flex-col gap-6"
+    headerClass="self-start md:px-0 p-0 text-2xl font-bold text-content text-ellipsis border-none"
+    bodyClass="p-0 md:p-0 md:pt-1 flex flex-col gap-6 border-none"
 >
-    <div class="mb-0 h-fit p-0">
-        <label
-            class="relative top-3 left-4 bg-white px-1 text-sm/6 font-normal text-black"
-            for="comment"
-        >
-            {$t("pages.checkout.verify.approved.formReview.commentModal.label")}
-        </label>
-        <textarea
-            class="border-secondary text-content h-30 w-full resize-none rounded-lg border bg-white p-4 text-base/6 font-normal"
-            id="comment"
-            bind:value={message}
-            placeholder={$t("pages.checkout.verify.approved.formReview.commentModal.label")}
-        ></textarea>
-    </div>
+    <TextArea
+        labelText={$t("pages.checkout.verify.approved.formReview.commentModal.label")}
+        bind:value={message}
+    />
     <Button onclick={handleSubmit} type={"submit"} class="w-full self-center md:w-fit md:self-end">
         {$t("common.save")}
     </Button>
