1cb3bc408f0cb1ff9f2cd9dd3766a4dfe6d8eea9
Angel Di Pinto <angel.dipinto@platoniq.net>
2026-09-01 13:12:38 +0200

    refactor: convert HomeBanners to svelte

---
+++ src/components/home/HomeBanners.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import HomeBanner from "./HomeBanner.svelte";
+    import { t } from "../../i18n/store";
+
+    import type { BannerRecord } from "../../repositories/banners";
+
+    interface Props {
+        banners: BannerRecord[];
+    }
+
+    let { banners }: Props = $props();
+</script>
+
+{#if banners.length > 0}
+    <section class="wrapper flex flex-col gap-4">
+        {#each banners as banner}
+            <HomeBanner
+                title={banner.title}
+                description={banner.content}
+                ctaText={banner.ctaText}
+                ctaLink={banner.ctaLink}
+                closeAriaLabel={$t("pages.home.banner.closeAriaLabel")}
+            />
+        {/each}
+    </section>
+{/if}
