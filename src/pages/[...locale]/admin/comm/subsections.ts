9917300468009af4c46df761b91154f592fb2f39
Dayv1dDev <david.moreno@platoniq.net>
2026-09-11 19:43:32 +0200

    chore: format code with prettier

--- src/pages/[...locale]/admin/comm/subsections.ts
+++ src/pages/[...locale]/admin/comm/subsections.ts
@@ -1,7 +1,8 @@
 import { ADMIN_SECTIONS } from "../../../../components/admin/adminSections";
 
-export const SUBSECTIONS = ADMIN_SECTIONS.find((section) => section.href === "/admin/comm")!
-    .subSections!.map((subSection) => ({
-        label: subSection.labelKey,
-        href: subSection.href,
-    }));
+export const SUBSECTIONS = ADMIN_SECTIONS.find(
+    (section) => section.href === "/admin/comm",
+)!.subSections!.map((subSection) => ({
+    label: subSection.labelKey,
+    href: subSection.href,
+}));
